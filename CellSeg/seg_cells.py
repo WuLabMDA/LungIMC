fe42c8664650fb48103f7b5831725cf15e51ce06
PingjunChen <pingjunchen@ieee.org>
2022-03-04 00:05:35 -0600

    update cell segmentation

--- CellSeg/seg_cells.py
+++ CellSeg/seg_cells.py
@@ -5,7 +5,9 @@ import argparse, shutil
 from skimage import io
 import numpy as np
 
+from deepcell.applications import Mesmer
 from deepcell.utils.plot_utils import create_rgb_image
+from deepcell.utils.plot_utils import make_outline_overlay
 
 
 def set_args():
@@ -35,9 +37,12 @@ if __name__ == "__main__":
         shutil.rmtree(segresult_dir)
     os.makedirs(segresult_dir)
 
+    # Initilize cell seg app
+    cellseg_app = Mesmer()
+
     # traverse patients one-by-one
     patient_list = sorted([ele for ele in os.listdir(segroi_dir)])
-    for ind, p_id in enumerate(patient_list[:3]):
+    for ind, p_id in enumerate(patient_list):
         print("Segment {:3d}/{:3d} ID {}".format(ind+1, len(patient_list), p_id))
         cur_p_dir = os.path.join(segroi_dir, p_id)
         input_roi_arr_list = []
@@ -47,10 +52,24 @@ if __name__ == "__main__":
             stain_list = [os.path.join(cur_roi_dir, ele + ".tif") for ele in [args.nuc_stain, args.mem_stain]]
             input_roi_arr_list.append(np.stack([io.imread(ele) for ele in stain_list], axis=2))
         input_roi_arrs = np.stack(input_roi_arr_list, axis=0).astype(np.float64)
-        # Vis original image
+
+        # Perform segmentation
+        seg_preds = cellseg_app.predict(input_roi_arrs, image_mpp=0.5)
+
+        # Save cell segmentation results
+        cur_roi_seg_dir = os.path.join(segresult_dir, p_id)
+        os.makedirs(cur_roi_seg_dir)
+        for idx, cur_roi_name in enumerate(cur_roi_list):
+            cur_roi_seg_path = os.path.join(cur_roi_seg_dir, cur_roi_name + ".npy")
+            np.save(cur_roi_seg_path, seg_preds[idx,:,:,0])
+
+        # Save visualization results
         rgb_images = create_rgb_image(input_roi_arrs, channel_colors=['green', 'blue'])
+        overlay_rois = make_outline_overlay(rgb_data=rgb_images, predictions=seg_preds)
         cur_roi_vis_dir = os.path.join(visroi_dir, p_id)
         os.makedirs(cur_roi_vis_dir)
-        for ind, cur_roi_name in enumerate(cur_roi_list):
-            cur_roi_vis_path = os.path.join(cur_roi_vis_dir, cur_roi_name + "_raw.png")
-            io.imsave(cur_roi_vis_path, rgb_images[ind, ...])
+        for idx, cur_roi_name in enumerate(cur_roi_list):
+            cur_roi_raw_path = os.path.join(cur_roi_vis_dir, cur_roi_name + "_raw.png")
+            io.imsave(cur_roi_raw_path, rgb_images[idx, ...])
+            cur_roi_overlay_path = os.path.join(cur_roi_vis_dir, cur_roi_name + "_seg.png")
+            io.imsave(cur_roi_overlay_path, overlay_rois[idx, ...])
