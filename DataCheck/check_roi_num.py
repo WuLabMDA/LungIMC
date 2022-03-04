fbfd4e5d9e048015fb62b0960db06e77d5391bd3
PingjunChen <pingjunchen@ieee.org>
2022-03-03 22:58:13 -0600

    add roi_num check

---
+++ DataCheck/check_roi_num.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import argparse
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description = "Filter Segmented Cells")
+    parser.add_argument("--data_root",        type=str,       default="/Data")
+    parser.add_argument("--data_type",        type=str,       default="Study")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = set_args()
+
+    roi_root_dir = os.path.join(args.data_root, args.data_type + "SlidesROIs")
+    slide_list = sorted([ele for ele in os.listdir(roi_root_dir) if os.path.isdir(os.path.join(roi_root_dir, ele))])
+    ttl_roi_num = 0
+    for cur_slide in slide_list:
+        cur_roi_dir = os.path.join(roi_root_dir, cur_slide)
+        roi_list = sorted([ele for ele in os.listdir(cur_roi_dir) if os.path.isdir(os.path.join(cur_roi_dir, ele))])
+        ttl_roi_num += len(roi_list)
+    print("{} has {} ROIs.".format(args.data_type, ttl_roi_num))
