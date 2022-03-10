1a5bd5d94efd9da5f70c790fdde717ad36b0d4a7
PingjunChen <pingjunchen@ieee.org>
2022-03-10 01:27:59 -0600

    update to aggregate_vis

--- CellSeg/aggregate_vis.py
+++ CellSeg/aggregate_vis.py
@@ -9,7 +9,7 @@ def set_args():
     parser.add_argument("--data_root",              type=str,       default="/Data")
     parser.add_argument("--data_type",              type=str,       default="Study", choices = ["Study", "Tonsil"])
     parser.add_argument("--vis_dir",                type=str,       default="VisROI")
-    parser.add_argument("--merge_dir",              type=str,       default="VisMerge")
+    parser.add_argument("--merge_dir",              type=str,       default="MergeSeg")
 
     args = parser.parse_args()
     return args
@@ -22,12 +22,14 @@ if __name__ == "__main__":
     vis_dir_root = os.path.join(roi_dir_root, args.vis_dir)
     # prepare folders for merge files
     merge_dir_root = os.path.join(roi_dir_root, args.merge_dir)
-    if os.path.exists(merge_dir_root):
-        shutil.rmtree(merge_dir_root)
     merge_raw_dir = os.path.join(merge_dir_root, "Raw")
+    if os.path.exists(merge_raw_dir):
+        shutil.rmtree(merge_raw_dir)
     os.makedirs(merge_raw_dir)
-    merge_seg_dir = os.path.join(merge_dir_root, "Seg")
-    os.makedirs(merge_seg_dir)
+    merge_overlay_dir = os.path.join(merge_dir_root, "Overlay")
+    if os.path.exists(merge_overlay_dir):
+        shutil.rmtree(merge_overlay_dir)
+    os.makedirs(merge_overlay_dir)
 
     # merge
     p_list = sorted([ele for ele in os.listdir(vis_dir_root) if os.path.isdir(os.path.join(vis_dir_root, ele))])
@@ -42,6 +44,6 @@ if __name__ == "__main__":
             src_seg_path = os.path.join(src_p_dir, roi_name + "_seg.png")
             dst_roi_name = roi_name[:roi_name.find("_")]
             dst_raw_path = os.path.join(merge_raw_dir, cur_p_name + dst_roi_name + ".png")
-            dst_seg_path = os.path.join(merge_seg_dir, cur_p_name + dst_roi_name + ".png")
+            dst_seg_path = os.path.join(merge_overlay_dir, cur_p_name + dst_roi_name + ".png")
             shutil.copyfile(src_raw_path, dst_raw_path)
             shutil.copyfile(src_seg_path, dst_seg_path)
