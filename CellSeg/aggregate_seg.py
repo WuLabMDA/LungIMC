007560be76d5b4e82f33d505fd03641b6759a0c3
PingjunChen <pingjunchen@ieee.org>
2022-03-10 02:15:15 -0600

    merge seg results to be roi-based manner

--- CellSeg/aggregate_seg.py
+++ CellSeg/aggregate_seg.py
@@ -22,6 +22,14 @@ if __name__ == "__main__":
     seg_dir_root = os.path.join(roi_dir_root, args.seg_dir)
     # prepare folders for merge files
     merge_dir_root = os.path.join(roi_dir_root, args.merge_dir)
+    merge_raw_dir = os.path.join(merge_dir_root, "Raw")
+    if os.path.exists(merge_raw_dir):
+        shutil.rmtree(merge_raw_dir)
+    os.makedirs(merge_raw_dir)
+    merge_overlay_dir = os.path.join(merge_dir_root, "Overlay")
+    if os.path.exists(merge_overlay_dir):
+        shutil.rmtree(merge_overlay_dir)
+    os.makedirs(merge_overlay_dir)
     merge_mask_dir = os.path.join(merge_dir_root, "Mask")
     if os.path.exists(merge_mask_dir):
         shutil.rmtree(merge_mask_dir)
@@ -35,8 +43,17 @@ if __name__ == "__main__":
         src_p_dir = os.path.join(seg_dir_root, cur_p)
         raw_img_list = sorted([ele for ele in os.listdir(src_p_dir) if ele.endswith(".npy")])
         for cur_img_name in raw_img_list:
+            # src filenames
             roi_name = cur_img_name[:-4]
-            src_seg_path = os.path.join(src_p_dir, roi_name + ".npy")
+            src_raw_path = os.path.join(src_p_dir, roi_name + "_raw.png")
+            src_overlay_path = os.path.join(src_p_dir, roi_name + "_seg.png")
+            src_mask_path = os.path.join(src_p_dir, roi_name + ".npy")
+            # dst filenames
             dst_roi_name = roi_name[:roi_name.find("_")]
-            dst_seg_path = os.path.join(merge_mask_dir, cur_p_name + dst_roi_name + ".npy")
-            shutil.copyfile(src_seg_path, dst_seg_path)
+            dst_raw_path = os.path.join(merge_raw_dir, cur_p_name + dst_roi_name + ".png")
+            dst_overlay_path = os.path.join(merge_overlay_dir, cur_p_name + dst_roi_name + ".png")
+            dst_mask_path = os.path.join(merge_mask_dir, cur_p_name + dst_roi_name + ".npy")
+            # copy
+            shutil.copyfile(src_raw_path, dst_raw_path)
+            shutil.copyfile(src_overlay_path, dst_overlay_path)
+            shutil.copyfile(src_mask_path, dst_mask_path)
