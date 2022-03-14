c6a349263c381734103e0ef036e37d043d4dd4af
PingjunChen <pingjunchen@ieee.org>
2022-03-13 23:19:47 -0500

    add load data for no control version

---
+++ BatchEffectCorrection/NoControl/load_data.R
@@ -0,0 +1,49 @@
+# load Panel metadata
+panel_file_path <- file.path(data_root, "BatchCorrection", "Metadata", "PanelsIMC.csv")
+imc_markers <- read.csv(panel_file_path) %>% pull(Marker)
+
+
+# load ROI metadata
+roi_meta_path <- file.path(data_root, "BatchCorrection", "Metadata", "NoControlMetaIMC.csv")
+roi_metadata <- suppressMessages(readr::read_csv(roi_meta_path))
+
+
+# load ROI cell features
+roi_fea_dir <- file.path(data_root, "StudyProcessing", "CellFeas")
+fea_filenames <- list.files(roi_fea_dir, pattern = "npy$")
+fea_filenames <- tools::file_path_sans_ext(fea_filenames)
+## Filter out ROIs outside the metadata
+fea_filenames <- fea_filenames[fea_filenames %in% roi_metadata[["Filename"]]]
+
+
+# load features
+file_num <- length(fea_filenames)
+np <- import("numpy")
+p_len <- 2
+p_num <- 35
+roi_fea_list <- vector(mode = "list", length = file_num)
+roi_nrows <- vector(length = file_num)
+for (ind in 1:file_num) {
+    cur_fea_path = file.path(roi_fea_dir, paste0(fea_filenames[ind], ".npy"))
+    cur_fea_mat <- tibble::as_tibble(np$load(cur_fea_path)[, (p_len+1):(p_len+p_num)])
+    roi_fea_list[[ind]] <- cur_fea_mat
+    roi_nrows[ind] <- nrow(cur_fea_mat)
+}
+
+# convert to feature matrix and add markers
+roi_feas <- bind_rows(roi_fea_list)
+colnames(roi_feas) <- imc_markers
+# add cell ids
+roi_feas$id <- 1:sum(roi_nrows)
+# add ROI ids
+roi_feas$sample <- fea_filenames %>% rep(roi_nrows)
+# add batch info
+batch_ids <- roi_metadata[["Batch"]][match(fea_filenames, roi_metadata[["Filename"]])] %>% as.factor() %>% rep(roi_nrows)
+roi_feas$batch <- batch_ids
+# add condition info
+condition_ids <- roi_metadata[["Condition"]][match(fea_filenames, roi_metadata[["Filename"]])] %>% as.factor() %>% rep(roi_nrows)
+roi_feas$condition <- condition_ids
+
+# transform data
+uncorrected <- transform_asinh(df = roi_feas, markers = imc_markers, cofactor = 5, .keep = TRUE)
+
