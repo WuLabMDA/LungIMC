a8961332d30cd011cc7733e72c679cfa5d6d5596
PingjunChen <pingjunchen@ieee.org>
2022-03-14 16:45:52 -0500

    update dump_data.R

--- BatchEffectCorrection/NoControl/dump_data.R
+++ BatchEffectCorrection/NoControl/dump_data.R
@@ -1,10 +1,11 @@
 # batch effect correction visualization
-vis_correction_dir <- file.path(data_root, "BatchCorrection", "NoControl")
+correct_dir <- file.path(data_root, "BatchCorrection")
+vis_correction_dir <- file.path(correct_dir, "NoControl")
 if (!dir.exists(vis_correction_dir))
     dir.create(vis_correction_dir, recursive = TRUE)
 
 # save objects
-no_control_meta_path <- file.path(vis_correction_dir, "meta.RData")
+no_control_meta_path <- file.path(vis_correction_dir, "Meta.RData")
 save(fea_filenames, markers, roi_nrows, file = no_control_meta_path)
 raw_fea_path <- file.path(vis_correction_dir, "RawFeas.RData")
 save(roi_feas, file = raw_fea_path)
