7d107761b503b047a2a42948c6f67c81350a1782
PingjunChen <pingjunchen@ieee.org>
2022-03-14 22:31:27 -0500

    add fastpg for transform

---
+++ BatchEffectCorrection/NoControl/fastpg_transform.R
@@ -0,0 +1,28 @@
+library(FastPG)
+community_name = "TransformedCommunities.RData"
+cell_feas <- select(uncorrected, CK, aSMA, CD31, CD45)
+cell_feas <- as.matrix(cell_feas)
+message(paste("Cell Number: ", NROW(cell_feas), " Feature Number: ", NCOL(cell_feas)))
+
+# Setup FastPG parameters
+k <- 30
+num_threads <- 32
+
+# Run FastPG
+message(paste("FastPG Start @ ", format(Sys.time(), "%a %b %d %X %Y")))
+clusters <- FastPG::fastCluster(cell_feas, k, num_threads)
+message(paste("FastPG Finish @ ", format(Sys.time(), "%a %b %d %X %Y")))
+
+# Analyze results
+communities <- clusters$communities
+unique_ids <- unique(communities)
+
+if(all(unique_ids >= 0)) {
+    message("No singleton clusters.")
+} else {
+    message("Singleton clusters exists!")
+}
+
+message(paste("There are ", length(unique_ids), " communties detected."))
+community_path <- file.path(vis_correction_dir, community_name)
+save(communities, file = community_path)
