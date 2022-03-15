2c10ff5990c9dd71eeea067bd623828ea1a2ed82
PingjunChen <pingjunchen@ieee.org>
2022-03-15 11:08:35 -0500

    rename to correct batch effects

--- BatchEffectCorrection/NoControl/correct_batcheffects.R
+++ BatchEffectCorrection/NoControl/correct_batcheffects.R
@@ -5,28 +5,29 @@ corrected <- batch_correct(df = uncorrected, covar = "condition", markers = mark
 labels <- corrected %>% create_som(markers = markers, rlen = 10)
 uncorrected$label <- corrected$label <- labels
 
+## save corrected features
+rdata_dir <- file.path(data_root, "BatchCorrection", "RData")
+if (!dir.exists(rdata_dir))
+    dir.create(rdata_dir, recursive = TRUE)
+CorrectedFeas <- corrected %>% data.frame()
+correct_fea_path <- file.path(rdata_dir, "TransformedFeas.csv")
+write.csv(CorrectedFeas, correct_fea_path, row.names = FALSE)
+
+
 # batch effect correction visualization
 vis_correction_dir <- file.path(data_root, "BatchCorrection", "NoControl")
 if (!dir.exists(vis_correction_dir))
     dir.create(vis_correction_dir, recursive = TRUE)
 
+# Evaluate MAD
+mad <- evaluate_mad(uncorrected, corrected, cell_col = "label")
+message(paste("MAD score is:", mad$score))
 # Evaluate EMD
-# mad <- evaluate_mad(uncorrected, corrected, cell_col = "label")
-# message(paste("MAD score is:", mad$score))
 emd <- evaluate_emd(uncorrected, corrected, cell_col = "label")
 message(paste("EMD Reduction is:", emd$reduction))
 png(file=file.path(vis_correction_dir, "emd_reduction.png"), width=1600, height=1000, res=180)
 cowplot::plot_grid(emd$violin, emd$scatterplot)
 dev.off()
-
-# # Violin plot
-# png(file=file.path(vis_correction_dir, "emd_reduction_violin.png"), width=1600, height=1000, res=300)
-# emd$violin
-# dev.off()
-# # Scatter plot
-# png(file=file.path(vis_correction_dir, "emd_scatterplot.png"), width=1600, height=1000, res=300)
-# emd$scatter
-# dev.off()
 
 # Create UMAPs
 sam <- sample(1:nrow(uncorrected), 50000)
@@ -37,6 +38,5 @@ plot2 <- plot_dimred(sample_corrected, "Corrected", type = "umap", plot = "batch
 # Umap plots
 plot_save_two(plot1, plot2, file.path(vis_correction_dir, "umap.png"))
 # Density plots
-plot_density(sample_uncorrected, sample_corrected, markers = markers,
-             filename = file.path(vis_correction_dir, "density.png"), 
-             y = "batch", ncol = 6)
+plot_density(sample_uncorrected, sample_corrected, markers = markers, y = "batch", ncol = 6,
+             filename = file.path(vis_correction_dir, "density.png"))
