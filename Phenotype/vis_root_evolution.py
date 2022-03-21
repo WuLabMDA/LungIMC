f61348447ccca37995e6d2cd3d45333bde5b4f2e
PingjunChen <pingjunchen@ieee.org>
2022-03-21 15:53:59 -0500

    add vis codes

---
+++ Phenotype/vis_root_evolution.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import numpy as np
+from datetime import datetime
+import argparse, shutil, pickle, pytz
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pyreadr
+from sklearn.manifold import TSNE
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description = "IMC Community Detection")
+    parser.add_argument("--data_root",              type=str,       default="/Data")
+    parser.add_argument("--batchcorrection_dir",    type=str,       default="BatchCorrection")
+    parser.add_argument("--vis_dir",                type=str,       default="Vis")
+    parser.add_argument("--fea_option",             type=str,       default="Transform", choices = ["Transform", "SelfCorrect", "ControlCorrect"])
+    parser.add_argument("--sample",                 default=True,   action="store_false")
+    parser.add_argument("--sample_cell_size",       type=int,       default=100000)
+    parser.add_argument("--seed",                   type=int,       default=1234)
+    args = parser.parse_args()
+    return args
+
+
+
+if __name__ == "__main__":
+    args = set_args()
+    # load data
+    cellfea_dir = os.path.join(args.data_root, args.batchcorrection_dir, "RData")
+    metadata_path = os.path.join(cellfea_dir, "StudyMeta.RData")
