4c5bc112ad2e8176d5f83f89f089f86985021537
PingjunChen <pingjunchen@ieee.org>
2022-03-29 18:30:56 -0500

    add three options

--- PreMatlab/S0_Setup.m
+++ PreMatlab/S0_Setup.m
@@ -6,5 +6,9 @@ addpath(genpath('./npymatlab'));
 
 % set variables
 data_root = 'E:\LungIMCData';
-raw_roi_dir = 'StudySlidesROIs';
-raw_process_dir = 'StudyProcessing';
+% raw_roi_dir = 'TonsilSlidesROIs';
+% raw_process_dir = 'TonsilProcessing';
+% raw_roi_dir = 'StudySlidesROIs';
+% raw_process_dir = 'StudyProcessing';
+raw_roi_dir = 'SupplSlidesROIs';
+raw_process_dir = 'SupplProcessing';
