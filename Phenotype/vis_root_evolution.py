1a8f55f77aaec34a6edd16f477762a85d8ce2a1f
PingjunChen <pingjunchen@ieee.org>
2022-03-29 12:29:44 -0500

    repalce vis to phenotype

--- Phenotype/vis_root_evolution.py
+++ Phenotype/vis_root_evolution.py
@@ -17,7 +17,7 @@ def set_args():
     parser = argparse.ArgumentParser(description = "IMC Community Detection")
     parser.add_argument("--data_root",              type=str,       default="/Data")
     parser.add_argument("--batchcorrection_dir",    type=str,       default="BatchCorrection")
-    parser.add_argument("--vis_dir",                type=str,       default="Vis")
+    parser.add_argument("--vis_dir",                type=str,       default="Phenotype")
     parser.add_argument("--fea_option",             type=str,       default="Transform", choices = ["Transform", "SelfCorrect", "ControlCorrect"])
     parser.add_argument("--sample",                 default=True,   action="store_false")
     parser.add_argument("--sample_cell_size",       type=int,       default=100000)
@@ -30,9 +30,8 @@ if __name__ == "__main__":
     args = set_args()
     # prepare directory
     vis_dir = os.path.join(args.data_root, args.vis_dir, args.fea_option)
-    if os.path.exists(vis_dir):
-        shutil.rmtree(vis_dir)
-    os.makedirs(vis_dir)
+    if not os.path.exists(vis_dir):
+        os.makedirs(vis_dir)
 
     # load metadata
     cellfea_dir = os.path.join(args.data_root, args.batchcorrection_dir, "RData")
