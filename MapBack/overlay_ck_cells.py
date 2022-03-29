af5a5fdbcf55882ce7141b4f5ce3f06c7ae226c7
PingjunChen <pingjunchen@ieee.org>
2022-03-29 11:53:57 -0500

    add a normal slide

--- MapBack/overlay_ck_cells.py
+++ MapBack/overlay_ck_cells.py
@@ -43,4 +43,31 @@ if __name__ == "__main__":
     key_roi_cells_path = os.path.join(filter_vis_dir, "{}_roi_cells.pkl".format(key_antibody))
     with open(key_roi_cells_path, "rb") as handle:
         roi_cells_dict = pickle.load(handle)
-    import pdb; pdb.set_trace()
+
+    # saving overlaid ck cells
+    for key in roi_cells_dict.keys():
+        cur_roi = key
+        slide_name = cur_roi[:-7]
+        # ADC slides: 2017-1G
+        # Normal slides: 2017-IL
+        if slide_name != "2017-1L":
+            continue
+        print("Overlay {}".format(cur_roi))
+        # load CK image
+        ck_data_path = os.path.join(stain_dir, cur_roi, "CK.tiff")
+        ck_data = tifffile.imread(ck_data_path)
+        ck_per99 = np.percentile(ck_data, 99)
+        ck_data[ck_data > ck_per99] = ck_per99
+        ck_data = (ck_data * 255.0 / ck_per99).astype(np.uint8)
+        ck_img = np.stack((ck_data, ck_data, ck_data), axis=2)
+        # load contours
+        roi_cnt_path = os.path.join(cnt_dir, cur_roi + ".h5")
+        roi_cnt_dict = dd.io.load(roi_cnt_path)
+        cell_cnt_dict = roi_cnt_dict["cell_cnt"]
+        # draw CK image
+        cell_list = roi_cells_dict[cur_roi]
+        for cell_id in cell_list:
+            cell_cnt = cell_cnt_dict[cell_id]
+            cv2.drawContours(ck_img, contours=[cell_cnt, ], contourIdx=0, color=[255, 0, 0], thickness=2)
+        cell_map_path = os.path.join(filter_overlay_dir, cur_roi + ".png")
+        io.imsave(cell_map_path, ck_img)
