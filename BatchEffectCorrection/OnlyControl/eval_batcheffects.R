1b5ce7a828116d70c90ffd91ddf505e8cb923267
PingjunChen <pingjunchen@ieee.org>
2022-03-30 12:10:11 -0500

    add batch effect evaluation on control

---
+++ BatchEffectCorrection/OnlyControl/eval_batcheffects.R
@@ -0,0 +1,13 @@
+# set batch effects evaluation directory
+batch_effects_dir <- file.path(data_root, "EvalControlBatchEffects")
+if (!dir.exists(batch_effects_dir))
+    dir.create(batch_effects_dir, recursive = TRUE)
+
+# checking for batch effects
+detect_batch_effect(uncorrected, out_dir = batch_effects_dir, downsample = NULL,
+                    norm_method = "scale", xdim = 8, ydim = 8,
+                    markers = markers, batch_col = "batch", seed = 1234)
+
+# quick detection of batch effects.
+detect_batch_effect_express(uncorrected, out_dir = batch_effects_dir,
+                            batch_col = "batch", downsample = NULL, seed = 1234)
---
+++ BatchEffectCorrection/OnlyControl/onlycontrol_setting.R
@@ -0,0 +1,7 @@
+# Load packages
+library(tidyverse)
+library(reticulate)
+library(cyCombine)
+
+# set data root
+data_root <- file.path("E:/LungIMCData", "BatchCorrection")
