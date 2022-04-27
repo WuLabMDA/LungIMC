96383962b8d36fc6f8872bf19b0ece59d41e8bcf
PingjunChen <pingjunchen@ieee.org>
2022-04-27 15:00:04 -0500

    add interested antibody heatmaps

--- Phenotype/tsne_root_phenotype.py
+++ Phenotype/tsne_root_phenotype.py
@@ -10,9 +10,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pyreadr
 from sklearn.manifold import TSNE
-# import umap
 
 from phenotype_utils import antibody_names, random_colors
+from phenotype_utils import interested_immune_antibodies, interested_nonimmnue_antibodies
 
 
 def set_args():
@@ -33,17 +33,15 @@ if __name__ == "__main__":
     random.seed(args.seed)
     np.random.seed(args.seed)
     print("Start @ {}".format(datetime.now(pytz.timezone('America/Chicago')).strftime("%m/%d/%Y, %H:%M:%S")))
-
     # prepare directory
     phenotype_dir = os.path.join(args.data_root, args.phenotype_dir, args.fea_option)
     if not os.path.exists(phenotype_dir):
         os.makedirs(phenotype_dir)
-
     # cluster info
     identified_cluster_path = os.path.join(args.data_root, args.phenotype_dir, "IdentifiedClusters.xlsx")
     cluster_info = pd.read_excel(identified_cluster_path, sheet_name=args.fea_option, header=0, index_col=None)
-    # cluster_dict = {cluster_id: category for cluster_id, category in zip(cluster_info["ClusterID"], cluster_info["Category"])}
-    cluster_dict = {cluster_id: category for cluster_id, category in zip(cluster_info["ClusterID"], cluster_info["VisConfirm"])}
+    cluster_dict = {cluster_id: category for cluster_id, category in zip(cluster_info["ClusterID"], cluster_info["Category"])}
+    # cluster_dict = {cluster_id: category for cluster_id, category in zip(cluster_info["ClusterID"], cluster_info["VisConfirm"])}
 
     # features & communites
     cell_feas, communities = None, None
@@ -83,25 +81,24 @@ if __name__ == "__main__":
         embed_feas = tsne.fit_transform(cell_feas)
         np.save(embed_feas_path, embed_feas)
 
-    # Merge tsne Plotting
-    fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(7, 5))
-    identified_communites = [cluster_dict[val] for val in communities]
-    merge_unique_ids = ["CK", "CD45", "Stroma"]
-    community_colors = [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)]
-    color_dict = {merge_unique_ids[ind]: (np.array(cur_color) * 255.0).astype(np.uint8) for ind, cur_color in enumerate(community_colors)}
-    cell_colors = [color_dict[val] for val in identified_communites]
-    hex_colors = ["#{:02x}{:02x}{:02x}".format(ele[0], ele[1], ele[2]) for ele in cell_colors]
-    axes.scatter(embed_feas[:, 0], embed_feas[:, 1], c=hex_colors, s=0.1)
-    axes.set_title("Merge Cell Distribution")
-    axes.set_xlim([-50, 50])
-    axes.set_ylim([-50, 50])
-    s_sne_name = "TSNE{}CellsMergeCommunities.png".format(cell_num)
-    t_sne_path = os.path.join(phenotype_dir, s_sne_name)
-    plt.savefig(t_sne_path, dpi=300)
-    plt.close()
+    # # Merge tsne Plotting
+    # fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(7, 5))
+    # identified_communites = [cluster_dict[val] for val in communities]
+    # merge_unique_ids = ["CK", "CD45", "Stroma"]
+    # community_colors = [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)]
+    # color_dict = {merge_unique_ids[ind]: (np.array(cur_color) * 255.0).astype(np.uint8) for ind, cur_color in enumerate(community_colors)}
+    # cell_colors = [color_dict[val] for val in identified_communites]
+    # hex_colors = ["#{:02x}{:02x}{:02x}".format(ele[0], ele[1], ele[2]) for ele in cell_colors]
+    # axes.scatter(embed_feas[:, 0], embed_feas[:, 1], c=hex_colors, s=0.1)
+    # axes.set_title("Merge Cell Distribution")
+    # axes.set_xlim([-50, 50])
+    # axes.set_ylim([-50, 50])
+    # s_sne_name = "TSNE{}CellsMergeCommunities.png".format(cell_num)
+    # t_sne_path = os.path.join(phenotype_dir, s_sne_name)
+    # plt.savefig(t_sne_path, dpi=300)
+    # plt.close()
 
-
-    # # # Interested Clusters
+    # # nterested Clusters
     # batch_num, cluster_num = 8, 8
     # interested_clusters = list(range(1+(batch_num-1)*cluster_num, 1+batch_num*cluster_num))
     # community_colors = random_colors(len(interested_clusters))
@@ -124,22 +121,27 @@ if __name__ == "__main__":
     # plt.savefig(t_sne_path, dpi=300)
     # plt.close()
 
-    # # Draw antibody heatmap
-    # fea_heatmap_dir = os.path.join(phenotype_dir, "{}CellsStainsHeatmap".format(cell_num))
-    # if os.path.exists(fea_heatmap_dir):
-    #     shutil.rmtree(fea_heatmap_dir)
-    # os.makedirs(fea_heatmap_dir)
-    # min_feas = np.percentile(cell_feas, q=1, axis=0)
-    # max_feas = np.percentile(cell_feas, q=99, axis=0)
-    # range_feas = max_feas - min_feas
-    # norm_feas = (cell_feas - min_feas) / range_feas
-    # norm_feas[norm_feas < 0] = 0
-    # norm_feas[norm_feas > 1] = 1.0
-    # for ind, fea_name in enumerate(antibody_names):
-    #     fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(7, 5))
-    #     axes.scatter(embed_feas[:, 0], embed_feas[:, 1], c=norm_feas[:, ind], s=0.1, cmap=plt.cm.jet)
-    #     axes.set_title("Feature: {}".format(fea_name))
-    #     plt.savefig(os.path.join(fea_heatmap_dir, "{}_heatmap.png".format(fea_name)), dpi=300)
-    #     plt.close()
-
+    # normalize features
+    min_feas = np.percentile(cell_feas, q=1, axis=0)
+    max_feas = np.percentile(cell_feas, q=99, axis=0)
+    range_feas = max_feas - min_feas
+    norm_feas = (cell_feas - min_feas) / range_feas
+    norm_feas[norm_feas < 0] = 0
+    norm_feas[norm_feas < 0.1] = 0 # ignore less than 0.1
+    norm_feas[norm_feas > 1] = 1.0
+    # Draw heatmaps of interested antibodies
+    interested_antibody_dict = {"Immnue": interested_immune_antibodies, "NonImmnue": interested_nonimmnue_antibodies}
+    for category, interested_antibodies in interested_antibody_dict.items():
+        fea_heatmap_dir = os.path.join(phenotype_dir, "{}-StainsHeatmap".format(category))
+        if os.path.exists(fea_heatmap_dir):
+            shutil.rmtree(fea_heatmap_dir)
+        os.makedirs(fea_heatmap_dir)
+        intereted_antibodies = []
+        for fea_name in interested_antibodies:
+            fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(7, 5))
+            ind = antibody_names.index(fea_name)
+            axes.scatter(embed_feas[:, 0], embed_feas[:, 1], c=norm_feas[:, ind], s=0.1, cmap=plt.cm.jet)
+            axes.set_title("Feature: {}".format(fea_name))
+            plt.savefig(os.path.join(fea_heatmap_dir, "{}_heatmap.png".format(fea_name)), dpi=300)
+            plt.close()
     print("Finish @ {}".format(datetime.now(pytz.timezone('America/Chicago')).strftime("%m/%d/%Y, %H:%M:%S")))
