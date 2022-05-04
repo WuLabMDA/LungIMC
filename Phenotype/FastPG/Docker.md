9f9bffed5898f54d988325f3490e04912ea7a236
PingjunChen <pingjunchen@ieee.org>
2022-05-04 00:19:28 -0500

    move deprecated code into Back

--- Phenotype/FastPG/Docker.md
+++
@@ -1,9 +0,0 @@
-### Run FastPG Docker Container
-```
-$ docker run -it --rm --user $(id -u):$(id -g) \
-  -v /rsrch1/ip/pchen6/LungIMCData:/Data \
-  -v /rsrch1/ip/pchen6/Codes/LungIMC:/Code/LungIMC \
-  -w /Code/LungIMC/Phenotype/FastPG \
-  --shm-size=240G --cpuset-cpus=0-39 \
-  --name fastpg_ping jefferys/fastpg:latest
-```
--- Phenotype/eval_consistency.m
+++
@@ -1,9 +0,0 @@
-data_root = 'E:\LungIMCData';
-% load communities
-raw_community_path = fullfile(data_root, 'BatchCorrection', 'RData', 'RawCommunities.txt');
-raw_list = importdata(raw_community_path);
-correct_community_path = fullfile(data_root, 'BatchCorrection', 'RData', 'CorrectCommunities.txt');
-correct_list = importdata(correct_community_path);
-cc = spx.cluster.ClusterComparison(raw_list, correct_list);
-fm = cc.fMeasure();
-spx.cluster.ClusterComparison.printF1MeasureResult(fm);
