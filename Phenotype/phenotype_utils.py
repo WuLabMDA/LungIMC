3d5102fb782a707fd0095f3b055fc70b37cb46bb
PingjunChen <pingjunchen@ieee.org>
2022-07-13 09:44:19 -0500

    rename Ir191_193 to Ir191

--- Phenotype/phenotype_utils.py
+++ Phenotype/phenotype_utils.py
@@ -8,10 +8,10 @@ import random, colorsys
 # Stain list
 antibody_names = ['B2M', 'B7_H3', 'CD11b', 'CD11c', 'CD14', 'CD163', 'CD19', 'CD31', 'CD33', 'CD3e',
                   'CD4', 'CD45', 'CD45RO', 'CD68', 'CD73', 'CD8a', 'CD94', 'CK', 'CTLA_4', 'FoxP3',
-                  'GranzymeB', 'HLA_DR', 'ICOS', 'IDO_1', 'Ir191_193', 'Ki67', 'LAG3', 'MPO', 'NaKATPase', 'PD_1',
+                  'GranzymeB', 'HLA_DR', 'ICOS', 'IDO_1', 'Ir191', 'Ki67', 'LAG3', 'MPO', 'NaKATPase', 'PD_1',
                   'PD_L1', 'TIGIT', 'TIM3', 'VISTA', 'aSMA']
 
-ordered_antibody_names = ['Ir191_193', 'NaKATPase', 'CD45', 'CD3e', 'CD8a', 'CD4', 'FoxP3',
+ordered_antibody_names = ['Ir191', 'NaKATPase', 'CD45', 'CD3e', 'CD8a', 'CD4', 'FoxP3',
                           'CD94', 'CD19', 'CD11b', 'CD11c', 'CD68', 'CD14', 'CD33',
                           'MPO', 'HLA_DR', 'B2M', 'CK', 'CD31', 'aSMA', 'Ki67',
                           'CD45RO','GranzymeB', 'LAG3', 'PD_1', 'PD_L1', 'TIGIT', 'TIM3',
