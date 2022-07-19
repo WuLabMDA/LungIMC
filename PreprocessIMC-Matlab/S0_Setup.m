5a18c4688863d8d98318a1f6f8e76430b245be58
PingjunChen <pingjunchen@ieee.org>
2022-07-18 21:53:13 -0500

    rename to PreprocessIMC-Matlab

---
+++ PreprocessIMC-Matlab/S0_Setup.m
@@ -0,0 +1,10 @@
+clearvars;
+
+% add libs
+addpath(genpath('./bfmatlab'));
+addpath(genpath('./npymatlab'));
+
+% set variables
+data_root = 'E:\LungIMCData';
+raw_roi_dir = 'LungSlidesROIs';
+raw_process_dir = 'LungROIProcessing';
