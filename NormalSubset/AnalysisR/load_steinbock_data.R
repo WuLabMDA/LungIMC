5a652d549d9b5c8b5189e265785bf4aa8fbb2afb
PingjunChen <pingjunchen@ieee.org>
2022-09-14 14:34:56 -0500

    add AnalysisR

---
+++ NormalSubset/AnalysisR/load_steinbock_data.R
@@ -0,0 +1,51 @@
+library(imcRtools)
+library(cytomapper)
+library(scater)
+library(stringr)
+library(dittoSeq)
+library(RColorBrewer)
+library(openxlsx)
+
+normal_root_dir <- "E:/LungIMCData/LungROIProcessing/SteinbockNormal"
+# Load steinbock generated data
+spe <- read_steinbock(normal_root_dir)
+
+# Set the colnames of the object to generate unique identifiers per cell
+colnames(spe) <- paste0(spe$sample_id, "_", spe$ObjectNumber)
+# Transform signal via arcsin hyperbolic
+assay(spe, "exprs") <- asinh(counts(spe)/5.0)
+# Define interesting channels
+rowData(spe)$use_channel <- !grepl("Ir191|NaKATPase|B2M", rownames(spe))
+# Embed expression via UMAP
+spe <- runUMAP(spe, subset_row = rowData(spe)$use_channel, exprs_values = "exprs")
+
+# add slide and roi information
+spe$slide_id <- str_extract_all(spe$sample_id, ".+(?=-ROI)", simplify = TRUE)
+spe$roi_id <- str_extract_all(spe$sample_id, "ROI[0-9]{3}", simplify = TRUE)
+
+# add location & diagnosis information
+roi_info <- read.xlsx("./StudyROI_Info.xlsx")
+spe$roi_location <- roi_info$ROI_Location[match(spe$sample_id, roi_info$ROI_ID)]
+spe$roi_diag <- roi_info$ROI_Diag[match(spe$sample_id, roi_info$ROI_ID)]
+
+# add slide-level patient and batch information
+slide_info <- read.xlsx("./StudySlide_Info.xlsx")
+spe$slide_diag <- slide_info$Slide_Diag[match(spe$slide_id, slide_info$Slide_ID)]
+spe$patient_id <- slide_info$Patient_ID[match(spe$slide_id, slide_info$Slide_ID)]
+spe$stain_id <- slide_info$Staining_ID[match(spe$slide_id, slide_info$Slide_ID)]
+
+# dittoRidgePlot(spe, var = "CK", group.by = "roi_location", assay = "exprs") +
+#     ggtitle("CK - after transformation")
+
+# Define color schemes
+color_vectors <- list()
+color_vectors$ROILoc <- setNames(brewer.pal(length(unique(spe$roi_location)), name = "Set1"), unique(spe$roi_location))
+color_vectors$ROIDiag <- setNames(brewer.pal(length(unique(spe$roi_diag)), name = "Set1"), unique(spe$roi_diag))
+color_vectors$SlideDiag <- setNames(brewer.pal(length(unique(spe$slide_diag)), name = "Set1"), unique(spe$slide_diag))
+color_vectors$PatientID <- setNames(colorRampPalette(brewer.pal(4, "PuOr"))(length(unique(spe$patient_id))), unique(spe$patient_id))
+color_vectors$Stain <- setNames(colorRampPalette(brewer.pal(4, "PuOr"))(length(unique(spe$stain_id))), unique(spe$stain_id))
+metadata(spe)$color_vectors <- color_vectors
+
+# Save generated SingleCellExperiment object
+spe_path = fullfile(normal_root_dir, "steinbock_spe.rds")
+saveRDS(spe, spe_path)
---
+++ NormalSubset/AnalysisR/seurat_correct.R
@@ -0,0 +1,47 @@
+library(patchwork)
+library(Seurat)
+library(SeuratObject)
+library(scater)
+library(cowplot)
+library(dittoSeq)
+library(viridis)
+set.seed(1234)
+
+normal_root_dir <- "E:/LungIMCData/LungROIProcessing/SteinbockNormal"
+raw_spe_path = fullfile(normal_root_dir, "steinbock_spe.rds")
+
+spe <- readRDS(raw_spe_path)
+
+# Convert to Seurat object
+seurat_obj <- as.Seurat(spe, counts = "counts", data = "exprs")
+seurat_obj <- AddMetaData(seurat_obj, as.data.frame(colData(spe)))
+seurat.list <- SplitObject(seurat_obj, split.by = "stain_id")
+
+# PCA -  finding anchors
+features <- rownames(spe)[rowData(spe)$use_channel]
+seurat.list <- lapply(X = seurat.list, FUN = function(x) {
+    x <- ScaleData(x, features = features, verbose = FALSE)
+    x <- RunPCA(x, features = features, verbose = FALSE)
+    return(x)
+})
+
+
+print(paste("---Start find integration anchors---"))
+anchors <- FindIntegrationAnchors(object.list = seurat.list,
+                                  anchor.features = features,
+                                  reduction = "rpca",
+                                  k.anchor = 20)
+print(paste("---Start integration---"))
+combined <- IntegrateData(anchorset = anchors)
+print(paste("Finish at", format(Sys.time(), "%a %b %d %X %Y")))
+
+# Select the integrated assay and perform PCA dimensionality reduction
+DefaultAssay(combined) <- "integrated"
+combined <- ScaleData(combined, verbose = FALSE)
+combined <- RunPCA(combined, npcs = 24, verbose = FALSE)
+reducedDim(spe, "seurat") <- combined@reductions$pca@cell.embeddings
+
+# Compute the UMAP embeddings based on Seurat integrated results
+spe <- runUMAP(spe, dimred = "seurat", name = "UMAP_seurat")
+seurat_spe_path = fullfile(normal_root_dir, "seurat_spe.rds")
+saveRDS(spe, seurat_spe_path)
