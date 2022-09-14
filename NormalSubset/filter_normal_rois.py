43c5c7b762df1e8427527abb42e8683eef27252b
PingjunChen <pingjunchen@ieee.org>
2022-09-14 13:45:27 -0500

    add filter rois

---
+++ NormalSubset/filter_normal_rois.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import shutil, argparse
+import pandas as pd
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description = "Filter ROIs")
+    parser.add_argument("--data_root",              type=str,       default="/Data")
+    parser.add_argument("--data_type",              type=str,       default="LungROIProcessing")
+    parser.add_argument("--raw_steinbock_dir",      type=str,       default="Steinbock")
+    parser.add_argument("--dst_steinbock_dir",      type=str,       default="SteinbockNormal")    
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = set_args()
+
+    raw_steinbock_dir = os.path.join(args.data_root, args.data_type, args.raw_steinbock_dir)
+    raw_roi_info_path = os.path.join(raw_steinbock_dir, "StudyROI_Info.xlsx")
+    raw_roi_df = pd.read_excel(raw_roi_info_path, index_col=None, sheet_name="Sheet1")
+    # filter Normal & DistantNormal ROIs
+    normal_roi_df = raw_roi_df[raw_roi_df["ROI_Location"].isin(["Normal", "DistantNormal"])]
+    dst_steinbock_dir = os.path.join(args.data_root, args.data_type, args.dst_steinbock_dir)
+    dst_roi_info_path = os.path.join(dst_steinbock_dir, "StudyROI_Info.xlsx")
+    normal_roi_df.to_excel(dst_roi_info_path, index=False, header=True)
