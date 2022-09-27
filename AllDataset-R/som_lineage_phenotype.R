ec78a53d0cb0c5d4ed4df6485d32aa23f441a765
PingjunChen <pingjunchen@ieee.org>
2022-09-27 14:39:21 -0500

    update cluster number to 10

--- AllDataset-R/som_lineage_phenotype.R
+++ AllDataset-R/som_lineage_phenotype.R
@@ -19,13 +19,13 @@ spe <- readRDS(raw_spe_path)
 lineage_strings <- "CD45|CD3e|CD4|FoxP3|CD8a|CD19|CD94|CD11b|CD11c|CD14|MPO|CD68|CD33|HLADR|CD45RO|CK|CD31|aSMA"
 rowData(spe)$lineage_markers <- grepl(lineage_strings, rownames(spe))
 
-# # Run FlowSOM and ConsensusClusterPlus clustering
-# spe <- cluster(spe, features = rownames(spe)[rowData(spe)$lineage_markers], maxK = 20, seed = 1234)
+# Run FlowSOM and ConsensusClusterPlus clustering
+spe <- cluster(spe, features = rownames(spe)[rowData(spe)$lineage_markers], maxK = 20, seed = 1234)
 
 # # Assess cluster stability
 # delta_area(spe)
 
 # # save som clustered results
-# spe$som_clusters <- cluster_ids(spe, "meta8")
+# spe$som_clusters <- cluster_ids(spe, "meta10")
 # phenotype_path <- file.path(data_root_dir, "som_raw_spe.rds")
 # saveRDS(spe, phenotype_path)
