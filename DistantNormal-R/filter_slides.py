afe69cccea66d125a36d0fb285a686ba050ce625
PingjunChen <pingjunchen@ieee.org>
2022-09-28 01:40:32 -0500

    update filtering distance normal

--- DistantNormal-R/filter_slides.py
+++ DistantNormal-R/filter_slides.py
@@ -9,7 +9,7 @@ def set_args():
     parser = argparse.ArgumentParser(description = "Filter ROIs")
     parser.add_argument("--data_root",              type=str,       default="/Data")
     parser.add_argument("--data_type",              type=str,       default="LungROIProcessing")
-    parser.add_argument("--raw_steinbock_dir",      type=str,       default="Steinbock")
+    parser.add_argument("--raw_steinbock_dir",      type=str,       default="SteinbockAll")
     parser.add_argument("--dst_steinbock_dir",      type=str,       default="SteinbockDistantNormal")    
 
     args = parser.parse_args()
@@ -19,11 +19,18 @@ def set_args():
 if __name__ == "__main__":
     args = set_args()
 
+    # load raw slide information
     raw_steinbock_dir = os.path.join(args.data_root, args.data_type, args.raw_steinbock_dir)
-    raw_roi_info_path = os.path.join(raw_steinbock_dir, "StudyROI_Info.xlsx")
-    raw_roi_df = pd.read_excel(raw_roi_info_path, index_col=None, sheet_name="Sheet1")
-    # filter Normal & DistantNormal ROIs
-    normal_roi_df = raw_roi_df[raw_roi_df["ROI_Location"] == "DistantNormal"]
+    raw_slide_info_path = os.path.join(raw_steinbock_dir, "StudySlide_Info.xlsx")
+    raw_slide_df = pd.read_excel(raw_slide_info_path, index_col=None, sheet_name="Sheet1")
+
     dst_steinbock_dir = os.path.join(args.data_root, args.data_type, args.dst_steinbock_dir)
     dst_roi_info_path = os.path.join(dst_steinbock_dir, "StudyROI_Info.xlsx")
-    normal_roi_df.to_excel(dst_roi_info_path, index=False, header=True)
+    dst_roi_df = pd.read_excel(dst_roi_info_path, index_col=None, sheet_name="Sheet1")
+    dst_roi_list = dst_roi_df["ROI_ID"].tolist()
+    normal_slide_set = [ele[:ele.find("-ROI")] for ele in dst_roi_list]
+    normal_slide_list = [ele for ele in set(normal_slide_set)]
+
+    dst_slide_df = raw_slide_df[raw_slide_df["Slide_ID"].isin(normal_slide_list)]
+    dst_slide_info_path = os.path.join(dst_steinbock_dir, "StudySlide_Info.xlsx")
+    dst_slide_df.to_excel(dst_slide_info_path, index=False, header=True) 
