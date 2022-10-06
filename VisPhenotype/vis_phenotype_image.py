712e4cc8cdbe8d5cfc1110ccf90fbf6fcaea0a2f
PingjunChen <pingjunchen@ieee.org>
2022-10-05 23:17:51 -0500

    update phenotype_image superimpose

--- VisPhenotype/vis_phenotype_image.py
+++ VisPhenotype/vis_phenotype_image.py
@@ -6,6 +6,9 @@ import pandas as pd
 import numpy as np
 from skimage import io
 import tifffile
+import cv2
+
+from seg_utils import bounding_box
 
 
 def set_args():
@@ -55,22 +58,37 @@ if __name__ == "__main__":
     # superimpose cells ontop antibodies
     for ind, (roi_name, cell_list) in enumerate(roi_id_dict.items()):
         print("Superimpose on ROI {} {}/{} ".format(roi_name, ind+1, len(roi_id_dict)))
-        cell_seg_path = os.path.join(cell_seg_dir, roi_name + ".tiff")
-        cell_seg = io.imread(cell_seg_path, plugin="tifffile").astype(np.int32)
+        if ind > 20:
+            continue
         img_list = []
         for antibody in antibody_list:
             cur_antibody_path = os.path.join(denoised_img_dir, roi_name, antibody + ".tiff")
             cur_antibody = io.imread(cur_antibody_path, plugin="tifffile").astype(np.float32)
             high_thresh = np.quantile(cur_antibody, 0.96)
             if high_thresh < 0.01:
-                cur_antibody = 0.0
+                cur_antibody = np.zeros_like(cur_antibody)
             else:
                 cur_antibody[cur_antibody > high_thresh] = high_thresh
                 cur_antibody[cur_antibody < 0.0] = 0.0
                 cur_antibody = cur_antibody * 1.0 / high_thresh
             img_list.append(cur_antibody)
-        img_arr = (np.asarray(img_list) * 255).astype(np.uint8)
-        import pdb; pdb.set_trace()
-
+        img_arr = (np.dstack(img_list) * 255).astype(np.uint8)
+        # overlay cell onto image
+        cell_seg_path = os.path.join(cell_seg_dir, roi_name + ".tiff")
+        cell_seg = io.imread(cell_seg_path, plugin="tifffile").astype(np.int32)
+        for cell_id in cell_list:
+            inst_map = np.array(cell_seg==cell_id, np.uint8)
+            y1 = y1 - 2 if y1 - 2 >= 0 else y1
+            x1 = x1 - 2 if x1 - 2 >= 0 else x1
+            x2 = x2 + 2 if x2 + 2 <= cell_seg.shape[1] - 1 else x2
+            y2 = y2 + 2 if y2 + 2 <= cell_seg.shape[0] - 1 else y2
+            inst_cell_crop = inst_map[y1:y2, x1:x2]
+            contours, hierarchy = cv2.findContours(inst_cell_crop, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+            contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+            img_crop = img_arr[y1:y2, x1:x2]
+            cv2.drawContours(img_crop, contours=contours, contourIdx=0, color=(180, 180, 180), thickness=1)
+            img_arr[y1:y2, x1:x2] = img_crop
+        cell_overlay_path = os.path.join(cell_phenotype_dir, roi_name + ".png")
+        io.imsave(cell_overlay_path, img_arr)
         
 
