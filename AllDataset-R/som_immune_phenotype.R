62f192695b71ec7cc32346338d01aa47cf59294f
PingjunChen <pingjunchen@ieee.org>
2022-10-19 22:51:00 -0500

    update phenotyping manners

--- AllDataset-R/som_immune_phenotype.R
+++ AllDataset-R/som_immune_phenotype.R
@@ -29,11 +29,11 @@ immnue_spe <- all_spe[, cell_indices]
 
 # Define lineage and function marker list
 # immnue_strings <- "CD45|CD3e|CD4|FoxP3|CD8a|CD19|CD94|CD11b|CD11c|CD14|MPO|CD68|CD33|HLADR|CD45RO|Ki67|CD163|ICOS|GranzymeB|TIM3|LAG3|VISTA|PD1|PDL1|TIGIT|IDO1|B7H3|CTLA4|CD73"
-immnue_strings <- "CD3e|^CD4$|FoxP3|CD8a|CD19|CD94|CD11b|CD11c|CD14|MPO|CD68|CD33"
+immnue_strings <- "CD45|CD3e|CD4|FoxP3|CD8a|CD19|CD94|CD11b|CD11c|CD14|MPO|CD68|CD33|HLADR|CD45RO"
 rowData(immnue_spe)$immnue_markers <- grepl(immnue_strings, rownames(immnue_spe))
 
 # Run FlowSOM and ConsensusClusterPlus clustering
-immnue_spe <- cluster(immnue_spe, features = rownames(immnue_spe)[rowData(immnue_spe)$immnue_markers], maxK = 20, seed = 1234)
+immnue_spe <- cluster(immnue_spe, features = rownames(immnue_spe)[rowData(immnue_spe)$immnue_markers], maxK = 50, seed = 1234)
 # # Assess cluster stability
 # delta_area(immnue_spe)
 
--- AllDataset-R/som_l1_phenotype.R
+++ AllDataset-R/som_l1_phenotype.R
@@ -16,16 +16,16 @@ raw_spe_path = file.path(data_root_dir, "all_steinbock_spe.rds")
 spe <- readRDS(raw_spe_path)
 
 # Define lineage marker list
-lineage_strings <- "CD45|CD3e|CD4|FoxP3|CD8a|CD19|CD94|CD11b|CD11c|CD14|MPO|CD68|CD33|HLADR|CD45RO|CK|CD31|aSMA"
-rowData(spe)$lineage_markers <- grepl(lineage_strings, rownames(spe))
+l1_antibodies <- "CD45|CD3e|CD4|FoxP3|CD8a|CD19|CD94|CD11b|CD11c|CD14|MPO|CD68|CD33|HLADR|CD45RO|CK|CD31|aSMA"
+rowData(spe)$l1_markers <- grepl(l1_antibodies, rownames(spe))
 
 # Run FlowSOM and ConsensusClusterPlus clustering
-spe <- cluster(spe, features = rownames(spe)[rowData(spe)$lineage_markers], maxK = 30, seed = 1234)
+spe <- cluster(spe, features = rownames(spe)[rowData(spe)$l1_markers], maxK = 20, seed = 1234)
 
 # # Assess cluster stability
 # delta_area(spe)
 
 # save som clustered results
-spe$som_clusters <- cluster_ids(spe, "meta11")
-phenotype_path <- file.path(data_root_dir, "som_lineage_spe.rds")
+spe$l1_clusters <- cluster_ids(spe, "meta11")
+phenotype_path <- file.path(data_root_dir, "som_l1_spe.rds")
 saveRDS(spe, phenotype_path)
