a456d86aeddaa895cb0bc078cf1107d7d7566873
PingjunChen <pingjunchen@ieee.org>
2022-11-18 15:50:02 -0600

    update path

---
+++ Steinbock/load_steinbock_data.R
@@ -0,0 +1,27 @@
+library(imcRtools)
+library(cytomapper)
+library(scater)
+library(stringr)
+library(dittoSeq)
+library(RColorBrewer)
+library(openxlsx)
+
+data_root_dir <- "E:/LungIMCData/HumanSampling41/LungROIProcessing/Steinbock"
+
+# Load steinbock generated data
+spe <- read_steinbock(data_root_dir)
+
+# Set the colnames of the object to generate unique identifiers per cell
+colnames(spe) <- paste0(spe$sample_id, "_", spe$ObjectNumber)
+# Transform signal via arcsin hyperbolic
+assay(spe, "exprs") <- asinh(counts(spe)/5.0)
+# Define interesting channels
+rowData(spe)$use_channel <- !grepl("Ir191|NaKATPase|B2M", rownames(spe))
+
+# add slide and roi information
+spe$slide_id <- str_extract_all(spe$sample_id, ".+(?=-ROI)", simplify = TRUE)
+spe$roi_id <- str_extract_all(spe$sample_id, "ROI[0-9]{3}", simplify = TRUE)
+
+# Save generated SingleCellExperiment object
+spe_file_path <- file.path(data_root_dir, "steinbock_spe.rds")
+saveRDS(spe, spe_file_path)
