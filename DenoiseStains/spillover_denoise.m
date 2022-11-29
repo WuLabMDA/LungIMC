4177f808b6fb274c5c079345263291eb59627b57
PingjunChen <pingjunchen@ieee.org>
2022-11-29 17:06:16 -0600

    increase aggegrate values

--- DenoiseStains/spillover_denoise.m
+++ DenoiseStains/spillover_denoise.m
@@ -1,9 +1,8 @@
 clearvars;
 
 % roi_root_dir = 'E:\LungIMCData\LungROIProcessing\Denoise';
-roi_root_dir = 'E:\LungIMCData\HumanWholeIMC\LungROIProcessing\Denoise';
-% roi_root_dir = 'E:\LungIMCData\HumanSampling-10-01\LungROIProcessing\Denoise';
-% roi_root_dir = 'E:\LungIMCData\HumanSampling35-0\LungROIProcessing\Denoise';
+% roi_root_dir = 'E:\LungIMCData\HumanWholeIMC\LungROIProcessing\Denoise';
+roi_root_dir = 'E:\LungIMCData\HumanSampling35-0\LungROIProcessing\Denoise';
 % roi_root_dir = 'E:\LungIMCData\HumanSampling35-1\LungROIProcessing\Denoise';
 % roi_root_dir = 'E:\LungIMCData\HumanSampling35-2\LungROIProcessing\Denoise';
 % roi_root_dir = 'E:\LungIMCData\HumanSampling35-3\LungROIProcessing\Denoise';
@@ -15,9 +14,9 @@ denoise_dir = fullfile(roi_root_dir, 'DenoisedROIs');
 stain_str_list = {'aSMA', 'B2M', 'B7_H3', 'CD3e', 'CD4', 'CD8a', 'CD11b', 'CD11c', 'CD14', 'CD19',...
     'CD31', 'CD33', 'CD45', 'CD45RO', 'CD68', 'CD73', 'CD94', 'CD163', 'CK', 'CTLA_4', 'FoxP3', 'GranzymeB',...
     'HLA_DR', 'ICOS', 'IDO_1', 'Ir191', 'Ki67', 'LAG3', 'MPO', 'NaKATPase', 'PD_1', 'PD_L1', 'TIGIT', 'TIM3', 'VISTA'};
-stain_pixel_num = [50, 50, 37, 5, 5, 25, 37, 5, 50, 5, ...
-    37, 5, 50, 37, 50, 50, 37, 37, 50, 35, 5, 5, ...
-    50, 5, 5, 25, 50, 25, 35, 50, 35, 30, 5, 5, 35];
+stain_pixel_num = [50, 50, 37, 30, 30, 30, 37, 25, 50, 20, ...
+    37, 25, 50, 50, 50, 50, 50, 37, 50, 35, 25, 25, ...
+    50, 25, 37, 25, 50, 25, 35, 50, 35, 30, 25, 25, 35];
 stain_agg_map = containers.Map(stain_str_list, stain_pixel_num);
 % quantile_val = 0.05;
 quantile_val_num = [0.95, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.85, 0.05, ...
@@ -51,10 +50,9 @@ for sind = 1:length(roi_list)
         bw_img = imbinarize(denoise_img);
         img_mask = bwareaopen(bw_img, stain_agg_map(stain_name));
         denoise_img(~img_mask) = 0;
-        % remove weak signal
-        d_thresh = quantile(denoise_img(denoise_img > 0.0), quantile_val_map(stain_name));
-        % d_thresh = quantile(denoise_img(denoise_img > 0.0), quantile_val);
-        denoise_img(denoise_img < d_thresh) = 0; 
+%         % remove weak signal
+%         d_thresh = quantile(denoise_img(denoise_img > 0.0), quantile_val_map(stain_name));
+%         denoise_img(denoise_img < d_thresh) = 0; 
         denoise_path = fullfile(denoise_roi_dir, stain_fullname);
         imwrite(denoise_img, denoise_path);
     end
