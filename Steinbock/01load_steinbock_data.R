e8219ee0285da28e60fa91e1f04a84362ac4b1f2
PingjunChen <pingjunchen@ieee.org>
2022-12-01 10:59:55 -0600

    update to 01load_steinbock_data

--- Steinbock/01load_steinbock_data.R
+++ Steinbock/01load_steinbock_data.R
@@ -17,8 +17,11 @@ spe <- read_steinbock(data_root_dir)
 
 # Set the colnames of the object to generate unique identifiers per cell
 colnames(spe) <- paste0(spe$sample_id, "_", spe$ObjectNumber)
+
 # Transform signal via arcsin hyperbolic
 assay(spe, "exprs") <- asinh(counts(spe)/5.0)
+# expr_vals <- assay(spe, "exprs")
+
 # Define interesting channels
 rowData(spe)$use_channel <- !grepl("Ir191|NaKATPase|B2M", rownames(spe))
 
