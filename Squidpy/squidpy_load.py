8296fdc900f568f23718d2f073059abff644c78d
PingjunChen <pingjunchen@ieee.org>
2023-01-30 00:59:18 -0600

    load data to squidpy format

--- Squidpy/squidpy_load.py
+++ Squidpy/squidpy_load.py
@@ -7,6 +7,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+import scanpy as sc
+import anndata
+import squidpy as sq
 
 
 def set_args():
@@ -14,13 +17,33 @@ def set_args():
     parser.add_argument("--data_root",              type=str,       default="/Data")
     parser.add_argument("--data_type",              type=str,       default="LungROIProcessing")
     parser.add_argument("--steinbock_dir",          type=str,       default="Steinbock")    
+    parser.add_argument("--object_name",            type=str,       default="objects.h5ad")    
 
     args = parser.parse_args()
     return args
 
 
-
 if __name__ == "__main__":
     args = set_args()
+
+    # directory setting
+    roi_root_dir = os.path.join(args.data_root, args.data_type)
+
+    # load cell phenotype information
+    cell_phenotype_path = os.path.join(roi_root_dir, "CellPhenotypes.json")
+    cell_phenotype_dict = None
+    with open(cell_phenotype_path) as fp:
+        cell_phenotype_dict = json.load(fp)
+    cell_ids = [ele for ele in cell_phenotype_dict.keys()]
+    cell_phenotypes = [ele for ele in cell_phenotype_dict.values()]     
+
+    steinbock_dir = os.path.join(roi_root_dir, args.steinbock_dir)
+    anndata_path = os.path.join(steinbock_dir, args.object_name)
+    imc_adata = sc.read_h5ad(anndata_path)
+    imc_adata.obs["cell_type"] = pd.Categorical(cell_phenotypes) 
+    import pdb; pdb.set_trace()  
+    # imc_ic = sq.im.ImageContainer.from_adata(imc_adata)
+    
+     
 
     
