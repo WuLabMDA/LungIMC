c9e08e3d8c9de12e9a32c6db4f8103a6d33a2b1a
PingjunChen <pingjunchen@ieee.org>
2023-02-16 22:39:54 -0600

    change T-reg-Cell to T-Reg-Cell

--- SpatialAnalysisR/update_spe_data.R
+++ SpatialAnalysisR/update_spe_data.R
@@ -9,11 +9,14 @@ raw_spe_name <- "steinbock_spe-all_celltypes_Renamed"
 raw_spe_path <- file.path(phenotype_dir, paste0(raw_spe_name, ".rds"))
 spe <- readRDS(raw_spe_path)
 
-### Add cell type information
-spe$celltype <- spe@metadata$celltypesRenamed
+### Update cell type information
+renamed_celltypes <- spe@metadata$celltypesRenamed
+renamed_celltypes[renamed_celltypes == "T-reg-Cell"] <- "T-Reg-Cell"
+spe$celltype <- renamed_celltypes
+
 
 ## save updated data
-updated_spe_name <- "lung_spe_15_celltypes"
+updated_spe_name <- "lung_spe_15_celltypes_final"
 updated_spe_path <- file.path(phenotype_dir, paste0(updated_spe_name, ".rds"))
 saveRDS(spe, file = updated_spe_path)
 
