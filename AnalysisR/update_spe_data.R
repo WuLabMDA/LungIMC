e74950d7ce49a647d2dc40c1ddbbb41d8dfb4edc
PingjunChen <pingjunchen@ieee.org>
2023-02-19 17:18:06 -0600

    comment saving to json

--- AnalysisR/update_spe_data.R
+++ AnalysisR/update_spe_data.R
@@ -14,18 +14,17 @@ renamed_celltypes <- spe@metadata$celltypesRenamed
 renamed_celltypes[renamed_celltypes == "T-reg-Cell"] <- "T-Reg-Cell"
 spe$celltype <- renamed_celltypes
 
-
 ## save updated data
 updated_spe_name <- "lung_spe_15_celltypes_final"
 updated_spe_path <- file.path(phenotype_dir, paste0(updated_spe_name, ".rds"))
 saveRDS(spe, file = updated_spe_path)
 
-## save to json
-cell_id_lst <- rownames(colData(spe))
-celltype_lst <- spe$celltype
-cell_df <- data.frame(
-    cell_id = cell_id_lst,
-    celltype = celltype_lst
-)
-celltype_csv_path <- file.path(phenotype_dir, "cell_phenotypes.csv")
-write.csv(cell_df, file=celltype_csv_path, row.names = FALSE)
+# ## save to json
+# cell_id_lst <- rownames(colData(spe))
+# celltype_lst <- spe$celltype
+# cell_df <- data.frame(
+#     cell_id = cell_id_lst,
+#     celltype = celltype_lst
+# )
+# celltype_csv_path <- file.path(phenotype_dir, "cell_phenotypes.csv")
+# write.csv(cell_df, file=celltype_csv_path, row.names = FALSE)
