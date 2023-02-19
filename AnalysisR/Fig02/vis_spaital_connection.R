ff00d897526f8631111838e3bbf3987c7f4daf2b
PingjunChen <pingjunchen@ieee.org>
2023-02-18 23:43:33 -0600

    update to vis_spatial_connectioN

--- AnalysisR/Fig02/vis_spaital_connection.R
+++ AnalysisR/Fig02/vis_spaital_connection.R
@@ -10,29 +10,27 @@ library(viridis)
 data_root_dir <- "E:/LungIMCData/HumanWholeIMC"
 phenotype_dir <- file.path(data_root_dir, "CellPhenotyping")
 
+## load spe object
 spe_celltype_name <-"lung_spe_15_celltypes_final"
 spe_celltype_path <- file.path(phenotype_dir, paste0(spe_celltype_name, ".rds"))
 spe <- readRDS(spe_celltype_path)
 
+# test roi
+test_roi_name <- "2166-1B-ROI009"
 
-# steinbock interaction graph
-test_roi_name <- "H17-0458-5-ROI016"
+# # cell visualization
+# plotSpatial(spe[, spe$sample_id == test_roi_name], 
+#             node_color_by = "celltype", 
+#             img_id = "sample_id", 
+#             node_size_fix = 0.5)
+
+# cell connection visualization
 plotSpatial(spe[, spe$sample_id == test_roi_name],
             node_color_by = "celltype",
             img_id = "sample_id",
+            node_size_fix = 0.5,
             draw_edges = TRUE,
             colPairName = "neighborhood",
             nodes_first = FALSE,
-            edge_color_fix = "grey") +
-    ggtitle("steinbock interaction graph")
-
-
-
-interaction_out <- testInteractions(spe, group_by = "sample_id",
-                                    label = "celltype", 
-                                    colPairName = "neighborhood", 
-                                    method = "patch",
-                                    patch_size = 3,
-                                    iter = 200)
-cell_type_spatial_path <- file.path(phenotype_dir, paste0("InteractionAnalysisIter200", ".RData"))
-save(spe, interaction_out, file = cell_type_spatial_path)
+            directed = FALSE,
+            edge_color_fix = "grey")
