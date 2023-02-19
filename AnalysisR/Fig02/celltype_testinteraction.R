ced029b46e861498155f8e536ee09801496bff75
PingjunChen <pingjunchen@ieee.org>
2023-02-19 00:47:17 -0600

    update code

--- AnalysisR/Fig02/celltype_testinteraction.R
+++ AnalysisR/Fig02/celltype_testinteraction.R
@@ -16,6 +16,7 @@ spe_celltype_path <- file.path(phenotype_dir, paste0(spe_celltype_name, ".rds"))
 spe <- readRDS(spe_celltype_path)
 
 ## test cell_type interaction
+print(paste("Start @", format(Sys.time(), "%a %b %d %X %Y")))
 interaction_out <- testInteractions(spe, group_by = "sample_id",
                                     label = "celltype", 
                                     colPairName = "neighborhood", 
@@ -24,3 +25,4 @@ interaction_out <- testInteractions(spe, group_by = "sample_id",
                                     iter = 200)
 cell_type_spatial_path <- file.path(phenotype_dir, paste0("InteractionsTestIter200", ".RData"))
 save(spe, interaction_out, file = cell_type_spatial_path)
+print(paste("Finish @", format(Sys.time(), "%a %b %d %X %Y")))
