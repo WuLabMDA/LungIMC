681ee3e9b86665b1f08b726d90420dc62bc4a3ce
PingjunChen <pingjunchen@ieee.org>
2023-03-01 23:01:53 -0600

    update slide_multivar_ratio comparison

--- AnalysisR/Fig02/RatioAnalysis/slide_multivar_ratio_compare.R
+++ AnalysisR/Fig02/RatioAnalysis/slide_multivar_ratio_compare.R
@@ -29,13 +29,17 @@ patient_info_name <- "Patient_Info"
 patient_info_path <- file.path(metadata_dir, paste0(patient_info_name, ".xlsx"))
 patient_df <- read_excel(patient_info_path)
 
+
 ## filtering rois
 roi_lst <- roi_df$ROI_ID
 roi_vec <- c()
 for (ind in 1:nrow(roi_df)) {
-    if (roi_df$ROI_Location[ind] == "Tumor" ) 
+    roi_location <- roi_df$ROI_Location[ind]
+    if (roi_location %in% c("Normal", "Tumor")) 
         roi_vec <- append(roi_vec, roi_df$ROI_ID[ind])
 }
+
+
 ## obtain cells inside tumor lesions
 lesion_indices = c()
 cell_id_lst <- rownames(colData(spe))
@@ -47,12 +51,14 @@ celltype_lst <- celltype_lst[lesion_indices]
 
 
 # set pathological stages (AAH/AIS/MIA/ADC)
-path_stage <- "ADC"
-cell_type <- "Proliferating-Cell"
+# path_stage <- "ADC"
+# cell_type <- "Proliferating-Cell"
+cell_type <- "CD8-T-Cell"
 
-# filtering stages
-stage_slide_df <- filter(slide_info_df, Slide_Diag==path_stage)
-stage_slide_lst <- stage_slide_df$Slide_ID
+# # filtering stages
+# stage_slide_df <- filter(slide_info_df, Slide_Diag==path_stage)
+# stage_slide_lst <- stage_slide_df$Slide_ID
+stage_slide_lst <- slide_info_df$Slide_ID
 
 
 ratio_lst <- c()
@@ -62,30 +68,38 @@ recurrent_lst <- c()
 median_age <- 71.3
 age_lst <- c()
 smoke_lst <- c()
+stage_lst <- c()
 
 for (ir in 1:length(stage_slide_lst)) {
     cur_slide <- stage_slide_lst[ir]
-    if (cur_slide == "H12-0330-2")
+    if (cur_slide == "2571-1D")
         next
+    # if (cur_slide == "H12-0330-2")
+    #     next    
     slide_cell_indices <- which(startsWith(cell_id_lst, cur_slide))
     slide_celltypes <- celltype_lst[slide_cell_indices]
     slide_cell_ratio <- sum(slide_celltypes == cell_type) / length(slide_celltypes)
     ratio_lst <- append(ratio_lst, slide_cell_ratio)
     underline_pos <- unlist(gregexpr("-", cur_slide))
     pat_id <- substr(cur_slide, 1, underline_pos[length(underline_pos)]-1)
-    df_index <- which(patient_df$PatientID == pat_id)
-    gender_lst <- append(gender_lst, patient_df$Gender[df_index])
-    race_lst <- append(race_lst, patient_df$Race[df_index])
-    if (patient_df$RecurrentStatus[df_index] == "RECURRENT")
+    patient_index <- which(patient_df$PatientID == pat_id)
+    gender_lst <- append(gender_lst, patient_df$Gender[patient_index])
+    race_lst <- append(race_lst, patient_df$Race[patient_index])
+    if (patient_df$RecurrentStatus[patient_index] == "RECURRENT")
         recurrent_lst <- append(recurrent_lst, "Recur")
     else
         recurrent_lst <- append(recurrent_lst, "Non-Recur")
-    if (patient_df$Age[df_index] <= median_age) 
+    if (patient_df$Age[patient_index] <= median_age) 
         age_lst <- append(age_lst, "<=71")
     else
         age_lst <- append(age_lst, ">71")
-    smoke_lst <- append(smoke_lst, patient_df$SmokeStatus[df_index])
+    smoke_lst <- append(smoke_lst, patient_df$SmokeStatus[patient_index])
+    slide_index <- which(slide_info_df$Slide_ID == cur_slide)
+    # if (length(slide_index) == 2)
+    #     print(cur_slide)
+    stage_lst <- append(stage_lst, slide_info_df$Slide_Diag[slide_index])
 }
+
 
 # MinMeanSEMMax
 MinMeanSEMMax <- function(x) {
@@ -94,11 +108,18 @@ MinMeanSEMMax <- function(x) {
     v
 }
 
-
 cell_ratio_df <- data.frame(Ratio=ratio_lst, Gender=gender_lst, Race=race_lst,
-                            Recurrent=recurrent_lst, Age=age_lst, Smoke=smoke_lst)
+                            Recurrent=recurrent_lst, Age=age_lst, Smoke=smoke_lst, Stage=stage_lst)
 long_ratio_df <- gather(cell_ratio_df, key = "variable", value = "value", -Ratio)
-ggplot(long_ratio_df, aes(x = value, y = Ratio) ) +
+variable_names <- c("<=71", ">71", "F", "M", "White", "Asian", "Non-Recur", "Recur", "Non-Smoker", "Smoker", "Normal", "AAH", "AIS", "MIA", "ADC")
+ggplot(long_ratio_df, aes(x = factor(value, level=variable_names), y = Ratio)) +
     stat_summary(fun.data=MinMeanSEMMax, geom="boxplot", colour="black") + 
-    geom_beeswarm(cex = 2.5, corral = "random", corral.width = 0.5) +
-    facet_wrap(~variable, scales = "free_x", ncol = 5)
+    geom_beeswarm(cex = 2.5, corral = "random", corral.width = 0.3)
+
+# Welch Two Sample t-test p-value
+age_ttest <- t.test(long_ratio_df$Ratio[long_ratio_df$value=="<=71"], long_ratio_df$Ratio[long_ratio_df$value==">71"])
+gender_ttest <- t.test(long_ratio_df$Ratio[long_ratio_df$value=="F"], long_ratio_df$Ratio[long_ratio_df$value=="M"])
+race_ttest <- t.test(long_ratio_df$Ratio[long_ratio_df$value=="White"], long_ratio_df$Ratio[long_ratio_df$value=="Asian"])
+recurrent_ttest <- t.test(long_ratio_df$Ratio[long_ratio_df$value=="Non-Recur"], long_ratio_df$Ratio[long_ratio_df$value=="Recur"])
+smoke_ttest <- t.test(long_ratio_df$Ratio[long_ratio_df$value=="Non-Smoker"], long_ratio_df$Ratio[long_ratio_df$value=="Smoker"])
+
