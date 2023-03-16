d625287d22dc98c664991728213888f0ecb83452
PingjunChen <pingjunchen@ieee.org>
2023-03-16 15:48:16 -0500

    update spatial connection visualization

--- AnalysisR/Fig02/VisCellGraph/vis_spatial_connection.R
+++ AnalysisR/Fig02/VisCellGraph/vis_spatial_connection.R
@@ -10,10 +10,17 @@ library(viridis)
 data_root_dir <- "E:/LungIMCData/HumanWholeIMC"
 phenotype_dir <- file.path(data_root_dir, "CellPhenotyping")
 
-## load spe object
-spe_celltype_name <-"lung_spe_15_celltypes_final"
-spe_celltype_path <- file.path(phenotype_dir, paste0(spe_celltype_name, ".rds"))
-spe <- readRDS(spe_celltype_path)
+# ## load spe object
+# spe_celltype_name <-"lung_spe_15_celltypes_final"
+# spe_celltype_path <- file.path(phenotype_dir, paste0(spe_celltype_name, ".rds"))
+# spe <- readRDS(spe_celltype_path)
+
+celltype_expansion_dir <- file.path(phenotype_dir, "ExpansionInteraction")
+threshold_val <- 24
+cell_type_interaction_name <- paste0("ExpansionInteractionThreshold", threshold_val, ".RData")
+cell_type_interaction_path <- file.path(celltype_expansion_dir, cell_type_interaction_name)
+cell_spatial_path <- file.path(cell_type_interaction_path)
+load(cell_spatial_path)
 
 # test roi
 test_roi_name <- "2166-1B-ROI009"
@@ -30,7 +37,7 @@ plotSpatial(spe[, spe$sample_id == test_roi_name],
             img_id = "sample_id",
             node_size_fix = 0.5,
             draw_edges = TRUE,
-            colPairName = "neighborhood",
+            colPairName = "expansion_interaction_graph",
             nodes_first = FALSE,
             directed = FALSE,
             edge_color_fix = "grey")
