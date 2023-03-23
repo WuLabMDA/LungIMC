aff8c37f2e68f22cbd29f3875aaffbe2a4678cac
PingjunChen <pingjunchen@ieee.org>
2023-03-23 13:51:24 -0500

    update Expansion Interaction

--- AnalysisR/Fig02/ExpansionCellInteraction/save_interaction_vals.R
+++ AnalysisR/Fig02/ExpansionCellInteraction/save_interaction_vals.R
@@ -9,7 +9,7 @@ library(stringr)
 data_root_dir <- "E:/LungIMCData/HumanWholeIMC"
 phenotype_dir <- file.path(data_root_dir, "CellPhenotyping")
 celltype_expansion_dir <- file.path(phenotype_dir, "ExpansionInteraction")
-threshold_val <- 24
+threshold_val <- 32
 cell_type_interaction_name <- paste0("ExpansionInteractionThreshold", threshold_val, ".RData")
 cell_type_interaction_path <- file.path(celltype_expansion_dir, cell_type_interaction_name)
 cell_spatial_path <- file.path(cell_type_interaction_path)
@@ -25,5 +25,6 @@ interaction_df <- data.frame(
     to_phenotypes = group_interaction$to_label,
     sig_vals = group_interaction$sigval
 )
-interaction_sigval_csv_path <- file.path(phenotype_dir, "interaction_sigval.csv")
+interaction_sigval_name <- paste0("ExpansionInteraction", threshold_val, "-Sigval.csv")
+interaction_sigval_csv_path <- file.path(celltype_expansion_dir, interaction_sigval_name)
 write.csv(interaction_df, file=interaction_sigval_csv_path, row.names = FALSE)
