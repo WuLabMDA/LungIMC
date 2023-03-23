f1c3c4c23830ed4f060758937cd64d31263d5013
PingjunChen <pingjunchen@ieee.org>
2023-03-23 12:06:43 -0500

    update parameter setting

--- AnalysisR/Fig02/CellularNeighborhood/cellular_neighborhood_heatmap.R
+++ AnalysisR/Fig02/CellularNeighborhood/cellular_neighborhood_heatmap.R
@@ -25,23 +25,25 @@ phenotype_dir <- file.path(data_root_dir, "CellPhenotyping")
 
 celltype_delaunay_dir <- file.path(phenotype_dir, "DelaunayInteraction")
 interaction_graph_name <- "delaunay_interaction_graph"
-threshold_val <- 30
+threshold_val <- 50
 cell_type_interaction_name <- paste0("DelaunayInteractionThreshold", threshold_val, ".RData")
 cell_type_interaction_path <- file.path(celltype_delaunay_dir, cell_type_interaction_name)
 load(cell_type_interaction_path)
 
 
 set.seed(1234)
+num_cn <- 10
 spe <- aggregateNeighbors(spe, colPairName = interaction_graph_name, 
                           aggregate_by = "metadata", count_by = "celltype")
-cn_kmeans <- kmeans(spe$aggregatedNeighbors, centers = 10)
+cn_kmeans <- kmeans(spe$aggregatedNeighbors, centers = num_cn)
 spe$cn_celltypes <- as.factor(cn_kmeans$cluster)
 
-# # save the spe with cellular neighborhood information
-# spe_cn_path <- file.path(data_root_dir, "SpatialAnalysis", "spe_cn10.rds")
-# saveRDS(spe, spe_cn_path)
+# save the spe with cellular neighborhood information
+spe_cn_name <- paste0("Delaunay", threshold_val, "-CN", num_cn, ".rds")
+spe_cn_path <- file.path(data_root_dir, "CellPhenotyping", spe_cn_name)
+saveRDS(spe, spe_cn_path)
 
 # Cellular neighborhood analysis heatmap
 for_plot <- prop.table(table(spe$cn_celltypes, spe$celltype), margin = 1)
-color_pal <- colorRampPalette(c("dark blue", "white", "dark red"))(10)
+color_pal <- colorRampPalette(c("dark blue", "white", "dark red"))(num_cn)
 pheatmap(for_plot, color = color_pal, scale = "column")
