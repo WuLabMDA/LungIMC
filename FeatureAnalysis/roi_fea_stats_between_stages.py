eb9e722d9f5c764dc3b7fdaeadb7d205e5b192ec
PingjunChen <pingjunchen@ieee.org>
2023-04-06 20:52:26 -0500

    feature selection

--- FeatureAnalysis/roi_fea_stats_between_stages.py
+++ FeatureAnalysis/roi_fea_stats_between_stages.py
@@ -2,12 +2,14 @@
 
 import os, sys
 import shutil, argparse
-import json, pytz
+import json, pytz, itertools
 from datetime import datetime
 import pandas as pd
 import numpy as np
 from scipy import stats
-
+import matplotlib.pyplot as plt
+from skimage import io
+import seaborn as sns
 
 def set_args():
     parser = argparse.ArgumentParser(description = "Visualize Cell Neighborhood")
@@ -37,7 +39,8 @@ if __name__ == "__main__":
     mia_adc_pval_lst = []
     
     # iterate each feature
-    common_features = []
+    increase_features = []
+    decrease_features = []
     for cur_fea_name in roi_fea_names:
         stage_dict = {}
         for cur_stage in roi_fea_df["ROI_Stage"].unique():
@@ -47,24 +50,58 @@ if __name__ == "__main__":
         ais_feas = stage_dict["AIS"]
         mia_feas = stage_dict["MIA"]
         adc_feas = stage_dict["ADC"]
-        p_normal_aah = stats.ttest_ind(normal_feas, aah_feas)
-        normal_aah_pval_lst.append(p_normal_aah.pvalue)
-        p_aah_ais = stats.ttest_ind(aah_feas, ais_feas)
-        aah_ais_pval_lst.append(p_aah_ais.pvalue)
-        p_ais_mia = stats.ttest_ind(ais_feas, mia_feas)
-        ais_mia_pval_lst.append(p_ais_mia.pvalue)
-        p_mia_adc = stats.ttest_ind(mia_feas, adc_feas)
-        mia_adc_pval_lst.append(p_mia_adc.pvalue)
+        normal_aah_p = stats.ttest_ind(normal_feas, aah_feas)
+        normal_aah_d = 1 if np.mean(aah_feas) > np.mean(normal_feas) else -1
+        normal_aah_pval_lst.append(normal_aah_p.pvalue)
+        aah_ais_p = stats.ttest_ind(aah_feas, ais_feas)
+        aah_ais_d = 1 if np.mean(ais_feas) > np.mean(aah_feas) else -1
+        aah_ais_pval_lst.append(aah_ais_p.pvalue)
+        ais_mia_p = stats.ttest_ind(ais_feas, mia_feas)
+        ais_mia_d = 1 if np.mean(mia_feas) > np.mean(ais_feas) else -1
+        ais_mia_pval_lst.append(ais_mia_p.pvalue)
+        mia_adc_p = stats.ttest_ind(mia_feas, adc_feas)
+        mia_adc_d = 1 if np.mean(adc_feas) > np.mean(mia_feas) else -1
+        mia_adc_pval_lst.append(mia_adc_p.pvalue)
         # find the maximum pval
-        max_pval = max(p_normal_aah.pvalue, p_aah_ais.pvalue, p_ais_mia.pvalue, p_mia_adc.pvalue)
-        if max_pval < 0.01:
-            common_features.append(cur_fea_name)
-    print("There are {} features with significance between featuers.".format(len(common_features)))
+        max_pval = max(normal_aah_p.pvalue, aah_ais_p.pvalue, ais_mia_p.pvalue, mia_adc_p.pvalue)
+        sum_d = normal_aah_d + aah_ais_d + ais_mia_d + mia_adc_d
+        # if max_pval < 0.1:
+        if sum_d == 4:
+            increase_features.append(cur_fea_name)
+        elif sum_d == -4:
+            decrease_features.append(cur_fea_name)
+            
+    # print("There are {} features significance increasing between featuers.".format(len(increase_features)))
+    # print(increase_features)
+    # print("There are {} features significance decreasing between featuers.".format(len(decrease_features)))
+    # print(decrease_features)
 
-    # # Save feature kruskal p-values
-    # fea_pval_df = pd.DataFrame(list(zip(roi_fea_names, normal_aah_pval_lst, aah_ais_pval_lst, 
-    #                                     ais_mia_pval_lst, mia_adc_pval_lst)), 
-    #                            columns=["Fea_Name", "Normal_AAH_Pval", "AAH_AIS_Pval", "AIS_MIA_Pval", "MIA_ADC_Pval"])
-    # fea_pval_path = os.path.join(feature_root_dir, "ROI_BetweenStage_Pvals.csv")
-    # fea_pval_df.to_csv(fea_pval_path, index=False)    
-    
+    roi_stages = [ele for ele in roi_fea_df["ROI_Stage"].tolist()]
+    stage_order_lst = ["Normal", "AAH", "AIS", "MIA", "ADC"]
+    for cur_stage in stage_order_lst:
+        print("{} has {} ROIs".format(cur_stage, sum([ele==cur_stage for ele in roi_stages])))
+
+    monotonous_features = list(itertools.chain(decrease_features, increase_features))
+    keep_features = ["ROI_ID", "ROI_Stage"]
+    keep_features.extend(monotonous_features)
+    keep_fea_df = roi_fea_df.loc[:, roi_fea_df.columns.isin(keep_features)]
+    keep_fea_df["ROI_Stage"] = pd.Categorical(keep_fea_df["ROI_Stage"], stage_order_lst)
+    keep_fea_df = keep_fea_df.sort_values("ROI_Stage")
+    keep_fea_df[monotonous_features] -= keep_fea_df[monotonous_features].min()
+    keep_fea_df[monotonous_features] /= keep_fea_df[monotonous_features].max()
+
+    monotonous_fea_arr = (np.transpose(keep_fea_df[monotonous_features].to_numpy()) * 255.0).astype(np.uint8)
+    cmap = plt.get_cmap('OrRd')
+    monotonous_fea_arr = cmap(monotonous_fea_arr)
+    monotonous_fea_map_path = os.path.join(feature_root_dir, "monotonous_fea_heatmap.pdf")
+    fig, ax = plt.subplots(figsize=(12, 6))
+    plt.imshow(monotonous_fea_arr)
+    plt.savefig(monotonous_fea_map_path, transparent=False, dpi=600)
+    # io.imsave(monotonous_fea_map_path, monotonous_fea_arr)
+
+    # stage_mean_df = keep_fea_df.groupby("ROI_Stage")[monotonous_features].mean()
+    # stage_mean_df = stage_mean_df.T
+    # fig, ax = plt.subplots(figsize=(3, 10))
+    # sns.heatmap(stage_mean_df, cmap='RdYlGn_r', linewidths=0.5, annot=False)
+    # monotonous_mean_map_path = os.path.join(feature_root_dir, "monotonous_fea_stage_mean_heatmap.png")
+    # plt.savefig(monotonous_mean_map_path, transparent=False, dpi=300)
--- FeatureAnalysis/roi_fea_stats_one_vs_rest.py
+++ FeatureAnalysis/roi_fea_stats_one_vs_rest.py
@@ -2,7 +2,7 @@
 
 import os, sys
 import shutil, argparse
-import json, pytz
+import json, pytz, itertools
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -31,11 +31,12 @@ if __name__ == "__main__":
     roi_fea_columns = [ele for ele in roi_fea_df.columns.tolist()]
     roi_fea_names = roi_fea_columns[2:] # exclude ROI_ID & ROI_Stage
 
-    normal_aah_pval_lst = []
-    aah_ais_pval_lst = []
-    ais_mia_pval_lst = []
-    mia_adc_pval_lst = []
-    
+    normal_pval_lst = []
+    aah_pval_lst = []
+    ais_pval_lst = []
+    mia_pval_lst = []
+    adc_pval_lst = []
+
     # iterate each feature
     common_features = []
     for cur_fea_name in roi_fea_names:
@@ -47,21 +48,26 @@ if __name__ == "__main__":
         ais_feas = stage_dict["AIS"]
         mia_feas = stage_dict["MIA"]
         adc_feas = stage_dict["ADC"]
-        import pdb; pdb.set_trace()
-        # rest_normal_feas = 
+        rest_normal_feas = list(itertools.chain(aah_feas, ais_feas, mia_feas, adc_feas))
+        rest_aah_feas = list(itertools.chain(normal_feas, ais_feas, mia_feas, adc_feas))
+        rest_ais_feas = list(itertools.chain(normal_feas, aah_feas, mia_feas, adc_feas))
+        rest_mia_feas = list(itertools.chain(normal_feas, aah_feas, ais_feas, adc_feas))
+        rest_adc_feas = list(itertools.chain(normal_feas, aah_feas, ais_feas, mia_feas))
 
-        p_normal_aah = stats.ttest_ind(normal_feas, aah_feas)
-        normal_aah_pval_lst.append(p_normal_aah.pvalue)
-        p_aah_ais = stats.ttest_ind(aah_feas, ais_feas)
-        aah_ais_pval_lst.append(p_aah_ais.pvalue)
-        p_ais_mia = stats.ttest_ind(ais_feas, mia_feas)
-        ais_mia_pval_lst.append(p_ais_mia.pvalue)
-        p_mia_adc = stats.ttest_ind(mia_feas, adc_feas)
-        mia_adc_pval_lst.append(p_mia_adc.pvalue)
-        # find the maximum pval
-        max_pval = max(p_normal_aah.pvalue, p_aah_ais.pvalue, p_ais_mia.pvalue, p_mia_adc.pvalue)
-        if max_pval < 0.01:
-            common_features.append(cur_fea_name)
-    print("There are {} features with significance between featuers.".format(len(common_features)))
+        normal_test = stats.ttest_ind(normal_feas, rest_normal_feas)
+        normal_pval_lst.append(normal_test.pvalue)
+        aah_test = stats.ttest_ind(aah_feas, rest_aah_feas)
+        aah_pval_lst.append(aah_test.pvalue)
+        ais_test = stats.ttest_ind(ais_feas, rest_ais_feas)
+        ais_pval_lst.append(ais_test.pvalue)
+        mia_test = stats.ttest_ind(mia_feas, rest_mia_feas)
+        mia_pval_lst.append(mia_test.pvalue)
+        adc_test = stats.ttest_ind(adc_feas, rest_adc_feas)
+        adc_pval_lst.append(adc_test.pvalue)
 
-    
+    # 
+    print("Normal has {} features with significance.".format(sum([(ele < 0.01) for ele in normal_pval_lst])))
+    print("AAH has {} features with significance.".format(sum([(ele < 0.01) for ele in aah_pval_lst])))
+    print("AIS has {} features with significance.".format(sum([(ele < 0.01) for ele in ais_pval_lst])))
+    print("MIA has {} features with significance.".format(sum([(ele < 0.01) for ele in mia_pval_lst])))
+    print("ADC has {} features with significance.".format(sum([(ele < 0.01) for ele in adc_pval_lst])))                
