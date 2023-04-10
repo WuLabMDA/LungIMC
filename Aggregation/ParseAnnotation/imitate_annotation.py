8ec1305fa62d51139bfbdc3f4b99889e092a2374
PingjunChen <pingjunchen@ieee.org>
2023-04-10 12:08:34 -0500

    add annotation imitation code

--- Aggregation/ParseAnnotation/imitate_annotation.py
+++ Aggregation/ParseAnnotation/imitate_annotation.py
@@ -35,8 +35,12 @@ if __name__ == "__main__":
         os.makedirs(imitation_dir)
 
     slide_lst = [os.path.splitext(ele)[0] for ele in os.listdir(annotation_dir) if ele.endswith(".svs")]
+    print("Number of slides: {}".format(len(slide_lst)))
     ann_slide_lst = [ele for ele in slide_lst if os.path.exists(os.path.join(annotation_dir, ele + ".xml"))]
-    # print("There are {} annotated slides.".format(len(ann_slide_lst)))
+    print("There are {} annotated slides.".format(len(ann_slide_lst)))
+    for cur_slide in slide_lst:
+        if cur_slide not in ann_slide_lst:
+            print(cur_slide)
 
     for ind, cur_slide in enumerate(ann_slide_lst):
         print("Imitate on {} {}/{}".format(cur_slide, ind+1, len(ann_slide_lst)))
@@ -46,29 +50,36 @@ if __name__ == "__main__":
         slide_width, slide_height = cur_slide_head.dimensions
         imitate_width = int(slide_width / args.reduction_ratio)
         imitate_height = int(slide_height / args.reduction_ratio)
-        imitate_img = np.zeros((imitate_height, imitate_width, 3), dtype=np.uint8)
         # load annotations
         cur_annotation_path = os.path.join(annotation_dir, cur_slide + ".xml")
         lesion_vertices, roi_anno_dict = parse_imagescope_annotations(cur_annotation_path)
         lesion_status = len(lesion_vertices) > 0
         if lesion_status:
+            lesion_img = np.zeros((imitate_height, imitate_width, 3), dtype=np.uint8)
             # draw lesion
             lesion_cv_cnt = np.expand_dims((lesion_vertices / args.reduction_ratio).astype(np.int32), axis=1)
-            cv2.drawContours(imitate_img, [lesion_cv_cnt, ], 0, (255, 0, 0), 3)
+            cv2.drawContours(lesion_img, [lesion_cv_cnt, ], 0, (255, 0, 0), -1)
         # draw rois
         # print("--- with {} ROIs".format(len(roi_anno_dict)))        
+        roi_img = np.zeros((imitate_height, imitate_width, 3), dtype=np.uint8)
         for roi_name, roi_cnt in roi_anno_dict.items():
             reduction_cnt = roi_cnt / args.reduction_ratio
             mean_x, mean_y = np.mean(reduction_cnt, axis=0)
-            cv2.putText(imitate_img, roi_name, (int(mean_x), int(mean_y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,  cv2.LINE_AA)
+            # # add roi information
+            # cv2.putText(imitate_img, roi_name, (int(mean_x), int(mean_y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,  cv2.LINE_AA)
             roi_cv_cnt = np.expand_dims(reduction_cnt.astype(np.int32), axis=1)
-            cv2.drawContours(imitate_img, [roi_cv_cnt, ], 0, (0, 0, 255), 2)
+            cv2.drawContours(roi_img, [roi_cv_cnt, ], 0, (0, 0, 255), -1)
 
         # Save Imitations
+        imitate_img = None
+        alpha = 0.3
         if lesion_status:
             anno_imitation_dir = os.path.join(imitation_dir, "Tumor")
+            imitate_img = cv2.addWeighted(lesion_img, alpha, roi_img, 1-alpha, 0)
         else:
             anno_imitation_dir = os.path.join(imitation_dir, "Normal")
+            imitate_img = roi_img
+
         if not os.path.exists(anno_imitation_dir):
             os.makedirs(anno_imitation_dir)
         # save imitation image
