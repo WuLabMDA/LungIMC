7a6873c47bee016673545a84d655f6fea104ee8e
PingjunChen <pingjunchen@ieee.org>
2023-04-27 14:20:33 -0500

    update roi feature comparison between stages

--- FeatureAnalysis/roi_fea_between_stages.py
+++ FeatureAnalysis/roi_fea_between_stages.py
@@ -16,6 +16,7 @@ def set_args():
     parser.add_argument("--data_root",              type=str,       default="/Data")
     parser.add_argument("--data_set",               type=str,       default="HumanWholeIMC", choices=["HumanWholeIMC", "HumanSampling35"])  
     parser.add_argument("--feature_dir",            type=str,       default="FeatureAnalysis")    
+    parser.add_argument("--pval_thresh",            type=float,     default=0.05)  
 
     args = parser.parse_args()
     return args
@@ -32,15 +33,16 @@ if __name__ == "__main__":
     roi_fea_df = pd.read_csv(roi_fea_path)
     roi_fea_columns = [ele for ele in roi_fea_df.columns.tolist()]
     roi_fea_names = roi_fea_columns[2:] # exclude ROI_ID & ROI_Stage
-    stage_order_lst = ["Normal", "AAH", "AIS_MIA", "ADC"]
-    # # print ROI number counts
-    # roi_stages = [ele for ele in roi_fea_df["ROI_Stage"].tolist()]
-    # for cur_stage in stage_order_lst:
-    #     print("{} has {} ROIs".format(cur_stage, sum([ele==cur_stage for ele in roi_stages])))    
+    stage_order_lst = ["Normal", "AAH", "AIS", "MIA", "ADC"]
+    # print ROI number counts
+    roi_stages = [ele for ele in roi_fea_df["ROI_Stage"].tolist()]
+    for cur_stage in stage_order_lst:
+        print("{} has {} ROIs".format(cur_stage, sum([ele==cur_stage for ele in roi_stages])))    
 
     normal_aah_pval_lst = []
-    aah_aismia_pval_lst = []
-    aismia_adc_pval_lst = []
+    aah_ais_pval_lst = []
+    ais_mia_pval_lst = []
+    mia_adc_pval_lst = []
     
     # iterate each feature
     increase_features = []
@@ -51,25 +53,33 @@ if __name__ == "__main__":
             stage_dict[cur_stage] = roi_fea_df[cur_fea_name][roi_fea_df["ROI_Stage"]==cur_stage].values
         normal_feas = stage_dict["Normal"]
         aah_feas = stage_dict["AAH"]
-        aismia_feas = stage_dict["AIS_MIA"]
+        ais_feas = stage_dict["AIS"]
+        mia_feas = stage_dict["MIA"]
         adc_feas = stage_dict["ADC"]
+
         normal_aah_p = stats.ttest_ind(normal_feas, aah_feas)
         normal_aah_pval_lst.append(normal_aah_p.pvalue)
         normal_aah_d = 1 if np.mean(aah_feas) > np.mean(normal_feas) else -1
-        aah_aismia_p = stats.ttest_ind(aah_feas, aismia_feas)
-        aah_aismia_pval_lst.append(aah_aismia_p.pvalue)
-        aah_aismia_d = 1 if np.mean(aismia_feas) > np.mean(aah_feas) else -1
-        aismia_adc_p = stats.ttest_ind(aismia_feas, adc_feas)
-        aismia_adc_d = 1 if np.mean(adc_feas) > np.mean(aismia_feas) else -1
-        aismia_adc_pval_lst.append(aismia_adc_p.pvalue)
 
+        aah_ais_p = stats.ttest_ind(aah_feas, ais_feas)
+        aah_ais_pval_lst.append(aah_ais_p.pvalue)
+        aah_ais_d = 1 if np.mean(ais_feas) > np.mean(aah_feas) else -1
+        
+        ais_mia_p = stats.ttest_ind(ais_feas, mia_feas)
+        ais_mia_pval_lst.append(ais_mia_p.pvalue)
+        ais_mia_d = 1 if np.mean(mia_feas) > np.mean(ais_feas) else -1
+
+        mia_adc_p = stats.ttest_ind(mia_feas, adc_feas)
+        mia_adc_pval_lst.append(mia_adc_p.pvalue)
+        mia_adc_d = 1 if np.mean(adc_feas) > np.mean(mia_feas) else -1
+        
         # find the maximum pval
-        max_pval = max(normal_aah_p.pvalue, aah_aismia_p.pvalue, aismia_adc_p.pvalue)
-        sum_d = normal_aah_d + aah_aismia_d + aismia_adc_d        
-        if max_pval < 0.01:
-            if sum_d == 3:
+        max_pval = max(normal_aah_p.pvalue, aah_ais_p.pvalue, ais_mia_p.pvalue, mia_adc_p.pvalue)
+        sum_d = normal_aah_d + aah_ais_d + ais_mia_d + mia_adc_d
+        if max_pval < args.pval_thresh:
+            if sum_d == 4:
                 increase_features.append(cur_fea_name)
-            elif sum_d == -3:
+            elif sum_d == -4:
                 decrease_features.append(cur_fea_name)
             
     print("There are {} features significance increasing between featuers.".format(len(increase_features)))
