c391f7c84dd289756d584352514fdf3b8737de18
PingjunChen <pingjunchen@ieee.org>
2023-05-02 10:53:33 -0500

    update lesion filtering

--- Aggregation/filter_lesion_rois.py
+++ Aggregation/filter_lesion_rois.py
@@ -21,17 +21,19 @@ if __name__ == "__main__":
     args = set_args()
 
     dataset_dir = os.path.join(args.data_root, args.data_set)
-    demo_root_dir = os.path.join(dataset_dir, "NatureFigures", "Fig03")
+    # load aggregated feature
+    feature_root_dir = os.path.join(dataset_dir, args.feature_dir)    
+    merge_roi_fea_path = os.path.join(feature_root_dir, "ROI_Fea_Aggregation.csv")
+    roi_fea_df = pd.read_csv(merge_roi_fea_path)
+    all_roi_ids = [ele for ele in roi_fea_df["ROI_ID"].tolist()]
 
     # load location information
+    demo_root_dir = os.path.join(dataset_dir, "NatureFigures", "Fig03")    
     imitation_dir = os.path.join(demo_root_dir, "Imitations")
     lesion_roi_loc_path = os.path.join(imitation_dir, "lesion_roi_loc.pkl")
     lesion_roi_dict = None
     with open(lesion_roi_loc_path, 'rb') as handle:
         lesion_roi_dict = pickle.load(handle)        
-
-    # load aggregated feature
-    feature_root_dir = os.path.join(dataset_dir, args.feature_dir)    
-    merge_roi_fea_path = os.path.join(feature_root_dir, "ROI_Fea_Aggregation.csv")
-    roi_fea_df = pd.read_csv(merge_roi_fea_path) 
-       
+    lesion_names = [ele for ele in lesion_roi_dict.keys()]
+    print("There are {} annotated leision.".format(len(lesion_names)))
+    # , 'ROI007':
