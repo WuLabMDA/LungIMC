28a8cca0606086afc450bbb0390aac0c932e27d6
PingjunChen <pingjunchen@ieee.org>
2023-05-22 12:42:01 -0500

    experiment weighting-based aggregation

--- TMB/volcano_slide_tmb.py
+++ TMB/volcano_slide_tmb.py
@@ -30,7 +30,8 @@ if __name__ == "__main__":
 
     dataset_dir = os.path.join(args.data_root, args.data_set)
     slide_tmb_dir = os.path.join(dataset_dir, args.tmb_dir)
-    lesion_fea_path = os.path.join(slide_tmb_dir, "lesion_avg_feas.csv")
+    # lesion_fea_path = os.path.join(slide_tmb_dir, "lesion_avg_feas.csv")
+    lesion_fea_path = os.path.join(slide_tmb_dir, "lesion_weigh_feas.csv")
     lesion_fea_df = pd.read_csv(lesion_fea_path)
 
     # normalize all features between 0.0 to 1.0
