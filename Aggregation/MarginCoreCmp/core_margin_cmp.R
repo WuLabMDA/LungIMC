10f8f1fec4966d830cf45f6310f22931993bfa30
PingjunChen <pingjunchen@ieee.org>
2023-06-26 17:25:34 -0500

    update core margin comparison

--- Aggregation/MarginCoreCmp/core_margin_cmp.R
+++ Aggregation/MarginCoreCmp/core_margin_cmp.R
@@ -7,26 +7,28 @@ adc_loc_fea_path <- file.path(data_root_dir, "adc_lesion_info.csv")
 adc_loc_fea <- read.csv(adc_loc_fea_path)
 
 # feature list
-study_fea_lst = c("CD8.T.Cell.Proportion", "CD8-T-Cell-Density", "Macrophage-Proportion", "Macrophage-Density", 
+study_fea_lst = c("CD8-T-Cell-Proportion", "CD8-T-Cell-Density", "Macrophage-Proportion", "Macrophage-Density", 
                  "B-Cell-Proportion", "B-Cell-Density", "Endothelial-Cell-Proportion", "Endothelial-Cell-Density",
                  "CD8-T-Cell-Epithelial-Cell", "Macrophage-Epithelial-Cell", "Endothelial-Cell-Fibroblast", "Neutrophil-T-Reg-Cell")
 
-# fea_name <- "CD8.T.Cell.Proportion"
-# fea_name <- "CD8.T.Cell.Density"
-# fea_name <- "Macrophage.Proportion"
-# fea_name <- "Macrophage.Density"
-# fea_name <- "B.Cell.Proportion"
-fea_name <- "Macrophage.Epithelial.Cell"
 
-margin_fea_df <- filter(adc_loc_fea, Loc == "TumorMargin")
-core_fea_df <- filter(adc_loc_fea, Loc == "TumorCore")
-margin_fea <- margin_fea_df[, fea_name]
-core_fea <- core_fea_df[, fea_name]
-cmp_df <- data.frame(Margin=margin_fea, Core=core_fea)
+for (cur_fea in study_fea_lst) {
+    fea_name <- gsub("-", ".", cur_fea)
+    margin_fea_df <- filter(adc_loc_fea, Loc == "TumorMargin")
+    core_fea_df <- filter(adc_loc_fea, Loc == "TumorCore")
+    margin_fea <- margin_fea_df[, fea_name]
+    core_fea <- core_fea_df[, fea_name]
+    cmp_df <- data.frame(Margin=margin_fea, Core=core_fea)
+    
+    # plot
+    p <- ggpaired(cmp_df, cond1 = "Margin", cond2 = "Core", fill = "condition", 
+             line.color = "gray", line.size = 0.4, palette = "npg") + ylab(fea_name)
+    cmp_plot_path <- file.path(data_root_dir, paste(cur_fea, "png", sep = "."))
+    png(cmp_plot_path)
+    print(p)
+    dev.off()
+}
 
-# plot
-ggpaired(cmp_df, cond1 = "Margin", cond2 = "Core", fill = "condition", 
-         line.color = "gray", line.size = 0.4, palette = "npg") +
-    ylab(fea_name)
+
 
 
