5004bc9ad00039d7d54cb2a38b1c09343a0bf2f6
PingjunChen <pingjunchen@ieee.org>
2023-07-25 17:06:54 -0500

    update immune density distribution

--- AnalysisR/Fig01/stage_lymphoid_immune_ratio_dist.R
+++ AnalysisR/Fig01/stage_lymphoid_immune_ratio_dist.R
@@ -65,4 +65,10 @@ MinMeanSEMMax <- function(x) {
 stage_order <- c("Normal", "AAH", "AIS", "MIA", "ADC")
 ggplot(stage_lymphoid_ratio_df, aes(x = factor(Stage, level=stage_order), y=Ratio)) + 
     stat_summary(fun.data=MinMeanSEMMax, geom="boxplot", colour="black") + 
-    geom_beeswarm(size=1.0) + ggtitle("Beeswarm")
+    geom_beeswarm(cex = 0.8, size=1.6)
+
+immune_density_plot_path <- file.path(data_root_dir, "NatureFigures", "Fig01", "Stage-Lymphoid-Immune-Ratio.pdf")
+ggsave(filename = immune_density_plot_path, device='pdf', width=8, height=8, dpi=300)
+while (!is.null(dev.list()))
+    dev.off()
+
