9dd7d702ee315a6c9d3e212701d6a90eedc7785f
PingjunChen <pingjunchen@ieee.org>
2023-08-10 15:27:01 -0500

    update spe lung subtype2

---
+++ AnalysisR/update_spe_cell_subtype2.R
@@ -0,0 +1,21 @@
+library(imcRtools)
+
+## set directory
+data_root_dir <- "E:/LungIMCData/HumanWholeIMC"
+phenotype_dir <- file.path(data_root_dir, "CellPhenotyping")
+
+## load raw data
+raw_spe_name <- "steinbock_spe-all_celltypes_Final2"
+raw_spe_path <- file.path(phenotype_dir, paste0(raw_spe_name, ".rds"))
+spe <- readRDS(raw_spe_path)
+
+### Update cell type information
+renamed_celltypes <- spe@metadata$celltypesRenamed
+renamed_celltypes[renamed_celltypes == "T-reg-Cell"] <- "T-Reg-Cell"
+spe$celltype <- renamed_celltypes
+spe$cellsubtype <- spe@metadata$celltypesRenamed2
+
+## save updated data
+updated_spe_name <- "lung_spe_32_cell_subtypes_final"
+updated_spe_path <- file.path(phenotype_dir, paste0(updated_spe_name, ".rds"))
+saveRDS(spe, file = updated_spe_path)
