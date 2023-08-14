130af045bae3516c7cf05e203a4925a550d2c2a6
PingjunChen <pingjunchen@ieee.org>
2023-08-14 11:41:37 -0500

    rename to plot_ith_sig_feas

--- Aggregation/ITH/plot_ith_sig_feas.py
+++ Aggregation/ITH/plot_ith_sig_feas.py
@@ -8,6 +8,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 from statsmodels.stats import multitest
+import matplotlib.pyplot as plt
 
 
 def set_args():
@@ -37,20 +38,45 @@ if __name__ == "__main__":
     lesion_fea_names = lesion_fea_columns[3:]
     
     # iterate each feature
+    sig_ith_fea_lst = []
     lesion_stage_lst = [ele for ele in slide_ith_df["LesionStage"].tolist()]
     for cur_fea in lesion_fea_names:
         aah_lst, ais_lst, mia_lst, adc_lst = [], [], [], []
         fea_ith_lst = [ele for ele in slide_ith_df[cur_fea].tolist()]
-        for cur_stage, cur_fea in zip(lesion_stage_lst, fea_ith_lst):
+        for cur_stage, cur_fea_val in zip(lesion_stage_lst, fea_ith_lst):
             if cur_stage == "AAH":
-                aah_lst.append(cur_fea)
+                aah_lst.append(cur_fea_val)
             elif cur_stage == "AIS":
-                ais_lst.append(cur_fea)
+                ais_lst.append(cur_fea_val)
             elif cur_stage == "MIA":
-                ais_lst.append(cur_fea)
+                mia_lst.append(cur_fea_val)
             elif cur_stage == "ADC":
-                ais_lst.append(cur_fea)
+                adc_lst.append(cur_fea_val)
             else:
                 sys.exit("Unknow stage: {}".format(cur_stage))
-        import pdb; pdb.set_trace()
-        
+        # Kruskal Wallis Test
+        kw_test = stats.kruskal(aah_lst, ais_lst, mia_lst, adc_lst)
+        if kw_test.pvalue < 0.05:
+            sig_ith_fea_lst.append(cur_fea)
+            # plot
+            fig, ax = plt.subplots(figsize =(10, 7))
+            bp = ax.boxplot([aah_lst, ais_lst, mia_lst, adc_lst])
+            ax.set_xticklabels(["AAH", "AIS", "MIA", "ADC"])
+            plt.title("{} ITH KW Test P-val: {}".format(cur_fea, kw_test.pvalue))
+
+            # prepare folder
+            fea_ith_plot_dir = os.path.join(lesion_ith_dir, "FeaITH")
+            if "CN" in cur_fea:
+                fea_ith_plot_dir = os.path.join(fea_ith_plot_dir, "CN")
+            else:
+                fea_ith_plot_dir = os.path.join(fea_ith_plot_dir, "CT")
+            if "Proportion" in cur_fea:
+                fea_ith_plot_dir = os.path.join(fea_ith_plot_dir, "Proportion")
+            elif "Density" in cur_fea:
+                fea_ith_plot_dir = os.path.join(fea_ith_plot_dir, "Density")
+            else:
+                fea_ith_plot_dir = os.path.join(fea_ith_plot_dir, "Others")
+            if not os.path.exists(fea_ith_plot_dir):
+                os.makedirs(fea_ith_plot_dir)
+            fea_ith_path = os.path.join(fea_ith_plot_dir, "{}.pdf".format(cur_fea))
+            plt.savefig(fea_ith_path, transparent=False, dpi=300)
